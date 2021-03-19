f9ed92e3395df121f52e0c382f379c828681147c
Igor <igorln96@gmail.com>
2021-03-19 18:44:04 -0300

    req 6,7,8

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
+import Comida from './pages/Comida';
 
 function App() {
   return (
     <Switch>
+      <Route path="/comidas" component={ Comida } />
       <Route exact path="/" component={ Login } />
     </Switch>
   );
---
+++ src/pages/Comida.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Comida = () => (
+  <h1>
+    Página comida
+  </h1>
+);
+
+export default Comida;

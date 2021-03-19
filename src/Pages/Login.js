bd6e84996e0bdd67cacdd64ac2c5a4398b4deae8
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-03-19 16:26:28 -0300

    requisito 5

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const checkValidity = () => {
+    const MIN_PASSWORD_LENGTH = 6;
+    if (password.length < MIN_PASSWORD_LENGTH) return false;
+    if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) return false;
+    return true;
+  };
+
   return (
     <div>
       <div>
         <input
           type="text"
           data-testid="email-input"
+          value={ email }
+          onChange={ (e) => setEmail(e.target.value) }
         />
         <input
           type="password"
           data-testid="password-input"
+          value={ password }
+          onChange={ (e) => setPassword(e.target.value) }
+
         />
       </div>
       <button
         type="button"
         data-testid="login-submit-btn"
+        disabled={ !checkValidity() }
       >
         Entrar
       </button>

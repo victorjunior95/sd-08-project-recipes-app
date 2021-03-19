e05aca9d60d6837ee731a6760ae9a50e5548fbaf
VitorLimaRios <vitorlimarios999@gmail.com>
2021-03-19 16:52:37 -0300

    [Group-4][Req-0] - Adicionando Rotas

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/login';
+import Comida from './pages/comida';
+import Bebida from './pages/bebida';
+import ComidaDetalhes from './pages/comida/detalhes';
+import BebidaDetalhes from './pages/bebida/detalhes';
+import BebidaProcesso from './pages/bebida/processo';
+import ComidaProcesso from './pages/comida/processo';
+import Explorar from './pages/explorar';
+import ExplorarComida from './pages/explorar/comida';
+import ExplorarBebida from './pages/explorar/bebida';
+import ComidaIngredientes from './pages/explorar/comida/ingredientes';
+import BebidaIngredientes from './pages/explorar/bebida/ingredientes';
+import ComidaArea from './pages/explorar/comida/area';
+import Perfil from './pages/perfil';
+import ReceitasFeitas from './pages/receitas-feitas';
+import ReceitasFavoritas from './pages/receitas-favoritas';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Switch>
+      <Route path="/receitas-favoritas" component={ ReceitasFavoritas } />
+      <Route path="/receitas-feitas" component={ ReceitasFeitas } />
+      <Route path="/perfil" component={ Perfil } />
+      <Route path="/explorar/comida/area" component={ ComidaArea } />
+      <Route path="/explorar/comida/ingredientes" component={ ComidaIngredientes } />
+      <Route path="/explorar/bebida/ingredientes" component={ BebidaIngredientes } />
+      <Route path="/explorar/comida" component={ ExplorarComida } />
+      <Route path="/explorar/bebida" component={ ExplorarBebida } />
+      <Route path="/explorar" component={ Explorar } />
+      <Route path="/bebidas/:idDaReceita/in-progress" component={ BebidaProcesso } />
+      <Route path="/comidas/:idDaReceita/in-progress" component={ ComidaProcesso } />
+      <Route path="/bebidas/:idDaReceita" component={ BebidaDetalhes } />
+      <Route path="/comidas/:idDaReceita" component={ ComidaDetalhes } />
+      <Route path="/bebidas" component={ Bebida } />
+      <Route path="/comidas" component={ Comida } />
+      <Route path="/" component={ Login } />
+    </Switch>
   );
 }
 

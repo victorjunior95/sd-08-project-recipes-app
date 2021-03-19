fbe15a4302df61ba2de8d6501d4b5fcd73857302
Vitor Nunes <vitornunes200@gmail.com>
2021-03-19 19:32:26 -0300

    Juncao dos requisitos feitos no dia

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React,{ useState} from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import profile from '../images/profileIcon.svg';

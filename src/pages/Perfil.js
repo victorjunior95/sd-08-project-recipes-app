088e0e99d351d7abc38ee4680ba82b8a0e48a735
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-03-19 19:48:18 -0300

    Pega o e-mail do redux

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Footer from '../components/Footer';
 import HeaderLocation from '../components/Header';
 
 class Perfil extends Component {
   render() {
+    const { email } = this.props;
     return (
       <div>
         <HeaderLocation />
         <main>
           <container className="perfil-container">
-            <span>e-mail</span>
+            <span>{email}</span>
             <button type="button">Receitas Feitas</button>
             <button type="button">Receitas Favoritas</button>
             <button type="button">Sair</button>
@@ -21,4 +24,12 @@ class Perfil extends Component {
   }
 }
 
-export default Perfil;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Perfil.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Perfil);

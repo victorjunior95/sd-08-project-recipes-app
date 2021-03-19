b73c8d19c2b0ed29e0c2c5ba7e58f29565b7c6fd
Doug-Fernando <doug.fernando95@gmail.com>
2021-03-19 17:35:53 -0300

    Req 1 a 8

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './components/Login';
+import Comidas from './components/Comidas';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={ Login } />
-        {/* <Route path="/comidas" />
-        <Route path="/bebidas" />
+        <Route exact path="/" render={ (props) => <Login { ...props } /> } />
+        <Route path="/comidas" component={ Comidas } />
+        {/* <Route path="/bebidas" />
         <Route path="/comidas/{id-da-receita}" />
         <Route path="/bebidas/{id-da-receita}" />
         <Route path="/comidas/{id-da-receita}/in-progress" />

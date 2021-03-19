9d12454a7369cb1b6f945ba8c7b096981b4a5935
Vitor Nunes <vitornunes200@gmail.com>
2021-03-19 19:02:02 -0300

    Req 11 - adicionada pagina perfil e funcao no icone de ir ao perfil

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Comidas from './pages/Comidas';
+import { Comidas, Bebidas, Login, Perfil } from './pages/index';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -9,8 +8,9 @@ function App() {
   return (
     <BrowserRouter>
       <Route path="/comidas" component={ Comidas } />
-      {/* <Route path="/bebidas" component={ Bebidas } />
-      <Route path="/comidas/:id" component={ DetalheComida } />
+      <Route path="/bebidas" component={ Bebidas } />
+      <Route path="/perfil" component={ Perfil } />
+      {/* <Route path="/comidas/:id" component={ DetalheComida } />
       <Route path="/bebidas/:id" component={ DetalheBebidas } />
       <Route path="/comidas/:id/in-progress" component={ InProgressComida } />
       <Route path="/bebidas/:id/in-progress" component={ InProgressBebidas } />
@@ -19,7 +19,6 @@ function App() {
       <Route path="/explorar/bebidas" component={ Explorar } />
       <Route path="/explorar/comidas/ingredientes" component={ Explorar } />
       <Route path="/explorar/bebidas/ingredientes" component={ Explorar } />
-      <Route path="/perfil" component={ Perfil } />
       <Route path="/receitas-feitas;" component={ Receitas } />
       <Route path="/receitas-favoritas" component={ Receitas } /> */}
       <Route exact path="/" component={ Login } />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import profile from '../images/profileIcon.svg';
 import search from '../images/searchIcon.svg';
@@ -8,7 +9,9 @@ const Header = (props) => {
   return (
     <header>
       <section className="header-bar">
-        <input type="image" src={ profile } alt="profile" data-testid="profile-top-btn" />
+        <Link to="/perfil">
+          <input type="image" src={ profile } alt="prof" data-testid="profile-top-btn" />
+        </Link>
         <h3 data-testid="page-title">{ title }</h3>
         <input type="image" src={ search } alt="search" data-testid="search-top-btn" />
       </section>

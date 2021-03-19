6c906155e58c81e4874ae24804f76a3df17d1d21
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2021-03-19 19:39:40 -0300

    routes

---
+++ src/pages/ProgressRecipesDrink.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+class ProgressRecipesDrink extends Component {
+  render() {
+    return (
+      <div>
+        Eu sou página de ProgressRecipesDrink
+      </div>
+    );
+  }
+}
+
+// Drinks.propTypes = {
+
+// };
+
+export default ProgressRecipesDrink;
---
+++ src/pages/ProgressRecipesFood.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+class ProgressRecipesFood extends Component {
+  render() {
+    return (
+      <div>
+        Eu sou página de ProgressRecipesFood
+      </div>
+    );
+  }
+}
+
+// Drinks.propTypes = {
+
+// };
+
+export default ProgressRecipesFood;

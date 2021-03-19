374773a0f13e158cc13d4f646065d9271ec3e67a
Victor Miwa <victoryukimiwa@hotmail.com>
2021-03-19 19:05:14 -0300

    correçao do lint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './Pages/Login';
 

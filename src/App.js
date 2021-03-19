5c7ecd6af2d535ed59ce61557e37dcf396fb13fe
rafasysop <rafaelcostamoura@gmail.com>
2021-03-19 16:23:44 -0300

    Comit Inicial

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="meals">
       <span className="logo">TRYBE</span>
+      <Card />
       <object
         className="rocksGlass"
         type="image/svg+xml"

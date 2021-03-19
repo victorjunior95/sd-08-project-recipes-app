4bc9d797c2663064ef787fd330820bc796b92a9e
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-03-19 20:05:14 -0300

    Adiciona redirecionamento para as rotas

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -1,23 +1,60 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { Redirect } from 'react-router';
 import Footer from '../components/Footer';
 import HeaderLocation from '../components/Header';
 
 class Perfil extends Component {
+  constructor() {
+    super();
+    this.state = {
+      routeTo: null,
+    };
+
+    this.setRoute = this.setRoute.bind(this);
+  }
+
+  setRoute(route) {
+    this.setState({ routeTo: route });
+  }
+
   render() {
     const { email } = this.props;
+    const { routeTo } = this.state;
     return (
       <div>
         <HeaderLocation />
         <main>
           <container className="perfil-container">
-            <span>{email}</span>
-            <button type="button">Receitas Feitas</button>
-            <button type="button">Receitas Favoritas</button>
-            <button type="button">Sair</button>
+            <span data-testid="profile-email">
+              {email
+            || JSON.parse(localStorage.getItem('user')).email}
+            </span>
+            <button
+              type="button"
+              data-testid="profile-done-btn"
+              onClick={ () => this.setRoute('/receitas-favoritas') }
+            >
+              Receitas Feitas
+            </button>
+            <button
+              type="button"
+              data-testid="profile-favorite-btn"
+              onClick={ () => this.setRoute('/receitas-feitas') }
+            >
+              Receitas Favoritas
+            </button>
+            <button
+              type="button"
+              data-testid="profile-logout-btn"
+              onClick={ () => this.setRoute('/') }
+            >
+              Sair
+            </button>
           </container>
         </main>
+        {routeTo && <Redirect push to={ routeTo } />}
         <Footer />
       </div>
     );

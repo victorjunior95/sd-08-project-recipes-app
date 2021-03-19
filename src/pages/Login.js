e364a33895c3b9d48094181be9a60f509f52605d
Bruno Pedrosa <bgpedrosas@gmail.com>
2021-03-19 17:12:40 -0300

    Tela de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const Login = () => <div>Login</div>;
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  useEffect(() => {
+    const verifyFieldsValidation = () => {
+      const MINIMAL_PASSWORD_CHARS = 6;
+
+      // Regex got from https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+      const emailRegex = new RegExp([
+        '^(([^<>()[\\]\\\\.,;:\\s@"]+(\\.[^<>()[\\]\\\\.,;:\\s@"]+)*)',
+        '|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])',
+        '|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$',
+      ].join(''));
+
+      const isEmailValid = emailRegex.test(email);
+      const isPasswordValid = password.length > MINIMAL_PASSWORD_CHARS;
+
+      setIsDisabled(!(isEmailValid && isPasswordValid));
+    };
+
+    verifyFieldsValidation();
+  }, [email, password]);
+
+  return (
+    <div>
+      <input
+        type="email"
+        data-testid="email-input"
+        placeholder="E-mail"
+        onChange={ ({ target: { value } }) => setEmail(value) }
+      />
+      <input
+        data-testid="password-input"
+        type="password"
+        placeholder="Senha"
+        onChange={ ({ target: { value } }) => setPassword(value) }
+      />
+      <button
+        type="submit"
+        data-testid="login-submit-btn"
+        disabled={ isDisabled }
+      >
+        Entrar
+      </button>
+    </div>
+  );
+};
 
 export default Login;

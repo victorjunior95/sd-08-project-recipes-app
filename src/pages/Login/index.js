fcb6c90667a1da402312f6444ed5faa881159378
UmberBn <humberto-bandeira@hotmail.com>
2021-03-19 19:47:24 -0400

    Corrigindo erro ao redirecionar da pagina inicial para a pagina de comidas

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,17 +1,21 @@
 import React, { useState } from 'react';
+import { Redirect } from 'react-router';
 import { setLocalStorage } from '../../services/localStorage';
 
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [redirect, setRedirect] = useState(false);
 
   const changeStorage = () => {
     const userEmail = { email };
     setLocalStorage('mealsToken', 1);
     setLocalStorage('cocktailsToken', 1);
     setLocalStorage('user', userEmail);
+    setRedirect(true);
   };
 
+  if (redirect) return <Redirect to="/comidas" />;
   return (
     <section>
       <label htmlFor="email-input">

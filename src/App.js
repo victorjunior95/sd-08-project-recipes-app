515e0333e1b27d7a0c78bc025a601b31c1bdaf64
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-03-19 19:14:03 -0300

    requisito 14

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Provider from './context/Provider';
+import Bebidas from './Pages/Bebidas';
+import Comidas from './Pages/Comidas';
 import Login from './Pages/Login';
+import Perfil from './Pages/Perfil';
 
 function App() {
   return (
     <Provider>
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route exact path="/comidas" component={ Comidas } />
+        <Route exact path="/bebidas" component={ Bebidas } />
+        <Route exact path="/perfil" component={ Perfil } />
       </Switch>
     </Provider>
   );

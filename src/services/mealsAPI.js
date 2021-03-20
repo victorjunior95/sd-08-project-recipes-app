02000ae71546d9953d6c1d2ae635479843fce914
hugoleonardodev <hugoleonardo.dev@gmail.com>
2021-03-20 17:49:54 -0300

    change end point mealsApi

--- src/services/mealsAPI.js
+++ src/services/mealsAPI.js
@@ -2,7 +2,7 @@
 // const FOOD_INGREDIENTS = 'https://www.themealdb.com/api/json/v1/1/filter.php?i={ingrediente}';
 // const FOOD_NAME = 'https://www.themealdb.com/api/json/v1/1/search.php?s={nome}';
 // const FOOD_FIRST_LETTER = 'https://www.themealdb.com/api/json/v1/1/search.php?f={primeira-letra}';
-const TEST = 'https://www.themealdb.com/api/json/v1/1/search.php?f=a';
+const TEST = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
 
 /*
   Se o radio selecionado for `Primeira letra` e a busca na API for feita com mais de uma letra,

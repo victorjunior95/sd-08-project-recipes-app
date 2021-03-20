851c6b7b725c5a24d10332a2677538148070bb5c
Aline Debastiani <debastianialine@gmail.com>
2021-03-19 22:34:09 -0300

    cumpre requisitos 2, 3 e 4

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,27 @@ import React from 'react';
 
 function Login() {
   return (
-    <div>oi</div>
+    <form>
+      <label htmlFor="email">
+        Email:
+        <input
+          data-testid="email-input"
+          type="text"
+          name="email"
+          placeholder="Email"
+        />
+      </label>
+      <label htmlFor="password">
+        Senha:
+        <input
+          data-testid="password-input"
+          type="password"
+          name="password"
+          placeholder="Senha"
+        />
+      </label>
+      <button type="submit" data-testid="login-submit-btn">Entrar</button>
+    </form>
   );
 }
 

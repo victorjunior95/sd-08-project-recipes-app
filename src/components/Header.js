3cc45823d035c474d13735f30807ecd417c1c04a
Clênio Galvão <clenio.cgm@gmail.com>
2021-03-20 10:59:45 -0300

    resolve questao 12

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,55 @@
-import React from 'react';
-import { Navbar, NavLink } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Form, InputGroup, Navbar, NavLink } from 'react-bootstrap';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 function Header() {
+  const [searchBar, setSearchBar] = useState(false);
+
+  function handleClick() {
+    setSearchBar(!searchBar);
+  }
+
   return (
-    <Navbar className="d-flex bg-warning justify-content-between align-items-center">
-      <NavLink
-        href="/perfil"
-        data-testids="profile-top-btn"
-        className="btn btn-warning border-0"
-      >
-        <img src={ profileIcon } className="img-fluid" alt="Profile Icon" />
-      </NavLink>
-      <span data-testids="page-title">pageTitle</span>
-      <NavLink
-        href="/explorar"
-        data-testids="profile-top-btn"
-        className="btn btn-warning border-0"
-      >
-        <img src={ searchIcon } className="img-fluid" alt="Search Icon" />
-      </NavLink>
-    </Navbar>
+    <div className="w-100">
+      <Navbar className="d-flex bg-warning justify-content-between align-items-center w-100">
+        <NavLink
+          href="/perfil"
+          data-testid="profile-top-btn"
+          className="btn btn-warning border-0"
+        >
+          <img src={ profileIcon } className="img-fluid" alt="Profile Icon" />
+        </NavLink>
+        <span data-testid="page-title">Comidas</span>
+        <NavLink
+          href="#"
+          onClick={ handleClick }
+          data-testid="search-top-btn"
+          className="btn btn-warning border-0"
+        >
+          <img src={ searchIcon } className="img-fluid" alt="Search Icon" />
+        </NavLink>
+      </Navbar>
+      { searchBar ? (
+        <Form>
+          <Form.Group controlId="formBasicEmail" >
+            <Form.Control type="text" placeholder="Buscar Receitas" data-testid="search-input"/>
+            <Form.Check inline name="radioPrefer" id="ingredientes">
+              <Form.Check.Input type="radio" />
+              <Form.Check.Label>Ingredientes</Form.Check.Label>
+            </Form.Check>
+            <Form.Check inline name="radioPrefer" id="nome">
+              <Form.Check.Input type="radio" />
+              <Form.Check.Label>Nome</Form.Check.Label>
+            </Form.Check>
+            <Form.Check inline name="radioPrefer" id="primeira letr">
+              <Form.Check.Input type="radio" />
+              <Form.Check.Label>Primeira letra</Form.Check.Label>
+            </Form.Check>
+            <Form.Control type="button" data-testid="exec-search-btn" />
+          </Form.Group>
+        </Form> ) : false }
+    </div>
   );
 }
 

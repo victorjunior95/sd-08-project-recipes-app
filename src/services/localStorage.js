326d546dc20d276d8d2268a844266a35927ba827
Iury Nathan <iurynathan@gmail.com>
2021-03-20 15:36:29 -0300

    fix function

--- src/services/localStorage.js
+++ src/services/localStorage.js
@@ -1,10 +1,10 @@
 // import md5 from 'crypto-js/md5'; // Biblioteca que gera Hash
 
 const setLocalStorage = (key, value) => {
-  if (typeof value !== 'string') {
-    if (value === 'email') {
+  if (typeof value === 'string') {
+    if (key === 'email') {
       const objEmail = { [key]: value };
-      localStorage.setItem('user', JSON.stringify(objEmail));
+      return localStorage.setItem('user', JSON.stringify(objEmail));
     }
     localStorage.setItem(key, JSON.stringify(value));
   }

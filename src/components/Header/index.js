7246bc92bc0fb3487db098f6b207ff5dc278fea5
UmberBn <humberto-bandeira@hotmail.com>
2021-03-20 22:00:30 -0400

    Criando um header com o titulo dinamico

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Navbar } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 import profileIcon from '../../images/profileIcon.svg';
 import searchIcon from '../../images/searchIcon.svg';
 
-export default function Header() {
+export default function Header(props) {
+  const { title } = props;
   return (
     <Navbar className="justify-content-around align-items-center bg-warning">
       <Navbar.Brand href="/perfil">
@@ -13,7 +15,7 @@ export default function Header() {
           data-testid="profile-top-btn"
         />
       </Navbar.Brand>
-      <h1 className="m-0 h5" data-testid="page-title">Cuscuz Redux</h1>
+      <h1 className="m-0 h5" data-testid="page-title">{title}</h1>
       <Navbar.Brand>
         <img
           src={ searchIcon }
@@ -24,3 +26,7 @@ export default function Header() {
     </Navbar>
   );
 }
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};

d2e6e922e01b3ad0ad37c964ab736781e979fa3a
hugoleonardodev <hugoleonardo.dev@gmail.com>
2021-03-21 04:14:32 -0300

    fix lint

--- src/containers/DrinkDetailsInProgress.jsx
+++ src/containers/DrinkDetailsInProgress.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const DrinkDetailsInProgress = () => {
   const [redirect, setRedirect] = useState(false);
--- src/containers/FoodDetailsInProgress.jsx
+++ src/containers/FoodDetailsInProgress.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const FoodDetailsInProgress = () => {
   const [redirect, setRedirect] = useState(false);

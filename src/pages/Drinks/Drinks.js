136528df52da115a177ef8bbd98afca8e63bafa2
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-03-21 16:25:04 -0300

    Requisito 16 aprovado

--- src/pages/Drinks/Drinks.js
+++ src/pages/Drinks/Drinks.js
@@ -1,23 +1,29 @@
 import React, { useContext } from 'react';
+import { Redirect } from 'react-router-dom';
 import Header from '../../components/Header';
 import Card from '../../components/Card';
 import { FoodCtx } from '../../context/ContextFood';
 
 function Drinks() {
+  const STOP_INDEX = 11;
   const { drinkApi: { drinks } } = useContext(FoodCtx);
   console.log(drinks);
   return (
     <div>
       <Header name="Bebidas" currentPage="Drinks" />
       <div className="cards">
-        {drinks && drinks.map((item) => (
-          <Card
-            key={ item.idDrink }
-            id={ item.idDrink }
-            name={ item.strDrink }
-            img={ item.strDrinkThumb }
-          />
-        ))}
+        {drinks && drinks
+          .filter((drink, index) => index <= STOP_INDEX)
+          .map((item) => (
+            <Card
+              key={ item.idDrink }
+              id={ item.idDrink }
+              name={ item.strDrink }
+              img={ item.strDrinkThumb }
+            />
+          ))}
+        { drinks && drinks.length === 1
+          ? <Redirect to={ `/bebidas/${drinks[0].idDrink}` } /> : '' }
       </div>
     </div>
   );
--- src/pages/Foods/Foods.js
+++ src/pages/Foods/Foods.js
@@ -1,23 +1,29 @@
 import React, { useContext } from 'react';
+import { Redirect } from 'react-router-dom';
 import './Foods.css';
 import Card from '../../components/Card';
 import Header from '../../components/Header';
 import { FoodCtx } from '../../context/ContextFood';
 
 function Foods() {
+  const STOP_INDEX = 11;
   const { foodApi: { meals } } = useContext(FoodCtx);
   return (
     <div>
       <Header name="Comidas" icon="true" currentPage="Foods" />
       <div className="cards">
-        {meals && meals.map((item) => (
-          <Card
-            key={ item.idMeal }
-            id={ item.idMeal }
-            name={ item.strMeal }
-            img={ item.strMealThumb }
-          />
-        ))}
+        {meals && meals
+          .filter((meal, index) => index <= STOP_INDEX)
+          .map((item) => (
+            <Card
+              key={ item.idMeal }
+              id={ item.idMeal }
+              name={ item.strMeal }
+              img={ item.strMealThumb }
+            />
+          ))}
+        { meals && meals.length === 1
+          ? <Redirect to={ `/comidas/${meals[0].idMeal}` } /> : '' }
       </div>
     </div>
   );

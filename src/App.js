befdabaa72206c9d44b82bd4708d023912f4ded4
Clênio Galvão <clenio.cgm@gmail.com>
2021-03-21 08:37:27 -0300

    Inicia questao 11

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
 import Comida from './pages/Comida';
+import Perfil from './pages/Perfil';
 
 function App() {
   return (
     <Switch>
       <Route path="/comidas" component={ Comida } />
+      <Route path="/perfil" component={ Perfil } />
       <Route exact path="/" component={ Login } />
     </Switch>
   );
---
+++ src/pages/Perfil.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const Perfil = () => (
+  <section className="w-100">
+    <Header />
+    <h1>
+      Perfil
+    </h1>
+  </section>
+);
+
+export default Perfil;

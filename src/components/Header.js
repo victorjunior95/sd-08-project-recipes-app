a007b6a6c4b5408c78dcf6243b5ef1c82d500ce5
Felipe França <felipefrancafj@gmail.com>
2021-03-22 16:46:32 -0300

    Padronizaçao de pastas no component Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
-import profileIcon from '../../assets/images/profileIcon.svg';
-import searchIcon from '../../assets/images/searchIcon.svg';
-import RecipesContext from '../../context/RecipesContext';
+import profileIcon from '../assets/images/profileIcon.svg';
+import searchIcon from '../assets/images/searchIcon.svg';
+import RecipesContext from '../context/RecipesContext';
 
 export default function Header() {
   const { title } = useContext(RecipesContext);

3c1c4297704d0b427ae74ca5500f0c3b96703802
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-03-22 13:12:39 -0300

    muda importaçao do BrowserRouter para o arquivo src/index.js para realizaçao dos testes unitarios corretamente

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Login from '../pages/Login';
 import { FoodDetails, FoodInProgress, Foods } from '../pages/Foods';
 import { Profile, FavRecipes, DoneRecipes } from '../pages/Profile';
@@ -15,32 +15,30 @@ import {
 
 function routes() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/comidas" exact component={ Foods } />
-        <Route path="/bebidas" exact component={ Drinks } />
-        <Route path="/comidas/:id" exact component={ FoodDetails } />
-        <Route path="/bebidas/:id" exact component={ DrinkDetails } />
-        <Route path="/comidas/:id/in-progress" component={ FoodInProgress } />
-        <Route path="/bebidas/:id/in-progress" component={ DrinkInProgress } />
-        <Route path="/explorar" exact component={ Explorer } />
-        <Route path="/explorar/comidas" exact component={ ExplorerFood } />
-        <Route path="/explorar/bebidas" exact component={ ExplorerDrink } />
-        <Route
-          path="/explorar/comidas/ingredientes"
-          component={ ExplorerFoodsIngredients }
-        />
-        <Route
-          path="/explorar/bebidas/ingredientes"
-          component={ ExplorerDrinksIngredients }
-        />
-        <Route path="/explorar/comidas/area" component={ ExplorerFoodsRegion } />
-        <Route path="/perfil" component={ Profile } />
-        <Route path="/receitas-feitas" component={ DoneRecipes } />
-        <Route path="/receitas-favoritas" component={ FavRecipes } />
-        <Route path="/" component={ Login } />
-      </Switch>
-    </BrowserRouter>
+    <Switch>
+      <Route path="/comidas" exact component={ Foods } />
+      <Route path="/bebidas" exact component={ Drinks } />
+      <Route path="/comidas/:id" exact component={ FoodDetails } />
+      <Route path="/bebidas/:id" exact component={ DrinkDetails } />
+      <Route path="/comidas/:id/in-progress" component={ FoodInProgress } />
+      <Route path="/bebidas/:id/in-progress" component={ DrinkInProgress } />
+      <Route path="/explorar" exact component={ Explorer } />
+      <Route path="/explorar/comidas" exact component={ ExplorerFood } />
+      <Route path="/explorar/bebidas" exact component={ ExplorerDrink } />
+      <Route
+        path="/explorar/comidas/ingredientes"
+        component={ ExplorerFoodsIngredients }
+      />
+      <Route
+        path="/explorar/bebidas/ingredientes"
+        component={ ExplorerDrinksIngredients }
+      />
+      <Route path="/explorar/comidas/area" component={ ExplorerFoodsRegion } />
+      <Route path="/perfil" component={ Profile } />
+      <Route path="/receitas-feitas" component={ DoneRecipes } />
+      <Route path="/receitas-favoritas" component={ FavRecipes } />
+      <Route path="/" component={ Login } />
+    </Switch>
   );
 }
 

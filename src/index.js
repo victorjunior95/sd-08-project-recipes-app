768f061932559e932287445c739dada46955bea1
Digo-Castro <dygo.castro@gmail.com>
2021-03-22 15:34:25 -0300

    Git Pull

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import RecipesContextProvider from './context/RecipesContextProvider';
 import App from './App';
-import RecipesContextProvider from './context/RecipesContextProvider';
 
 ReactDOM.render(
   <RecipesContextProvider>

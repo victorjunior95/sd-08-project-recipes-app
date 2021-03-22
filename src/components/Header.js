f4cbfb8f632876d07503b78d3108fd5ff9e423a7
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-03-22 12:46:38 -0300

    cria estilo para o header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,30 +5,35 @@ import seachIcon from '../images/searchIcon.svg';
 import profileIcon from '../images/profileIcon.svg';
 import SearchBar from './SearchBar';
 
+import '../styles/Header.css';
+
 function Header({ title, search }) {
   const [renderSeachBar, setRenderSearchBar] = useState(false);
   return (
-    <header>
-      <Link to="/perfil">
-        <img
-          data-testid="profile-top-btn"
-          src={ profileIcon }
-          alt=""
-        />
-      </Link>
-      <h1 data-testid="page-title">
-        {title}
-      </h1>
-      {search === false
-        ? ''
-        : (
-          <input
-            onClick={ () => setRenderSearchBar(!renderSeachBar) }
-            type="image"
-            data-testid="search-top-btn"
-            src={ seachIcon }
+    <header className="header">
+      <div className="header-content">
+        <Link to="/perfil">
+          <img
+            data-testid="profile-top-btn"
+            src={ profileIcon }
             alt=""
-          />)}
+          />
+        </Link>
+        <h1 data-testid="page-title">
+          {title}
+        </h1>
+        {search === false
+          ? ''
+          : (
+            <input
+              onClick={ () => setRenderSearchBar(!renderSeachBar) }
+              type="image"
+              data-testid="search-top-btn"
+              src={ seachIcon }
+              alt=""
+            />)}
+
+      </div>
       {renderSeachBar && <SearchBar />}
     </header>
   );

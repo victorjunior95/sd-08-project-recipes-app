860214e34abf51de9d34e4d039b5316463b8defc
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2021-03-22 16:26:12 -0300

    requisito 5

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from '../components/Input';
 import Button from '../components/Button';
 
 const Login = () => {
-  console.log('login');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const checkEmailAndPassword = () => {
+    const minimumPasswordSize = 6;
+    const re = /.+@[A-z]+[.]com/;
+    const isValidEmail = re.test(email);
+    const isValidPassword = password.length > minimumPasswordSize;
+    if (isValidPassword && isValidEmail) {
+      return false;
+    }
+    return true;
+  };
+
+  const emailChange = ({ target: { value } }) => {
+    setEmail(value);
+  };
+
+  const passwordChange = ({ target: { value } }) => {
+    setPassword(value);
+  };
+
   return (
     <form>
       <Input
@@ -11,14 +32,20 @@ const Login = () => {
         type="email"
         placeholder="Email"
         data-testid="email-input"
+        onChange={ emailChange }
       />
       <Input
         name="password"
         type="password"
         placeholder="Senha"
         data-testid="password-input"
+        onChange={ passwordChange }
       />
-      <Button name="Entrar" data-testid="login-submit-btn" />
+      <Button
+        name="Entrar"
+        data-testid="login-submit-btn"
+        disabled={ checkEmailAndPassword() }
+      />
     </form>
   );
 };

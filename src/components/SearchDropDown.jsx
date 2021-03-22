03f06ef93cdb62b8bdb60bc4fb7254cf7572f526
Cainan <cainan6697@gmail.com>
2021-03-22 19:42:29 -0300

    Fix CSS

--- src/components/SearchDropDown.jsx
+++ src/components/SearchDropDown.jsx
@@ -11,6 +11,7 @@ function SearchDropDown() {
       <div className="radio-container">
         <label className="radio-button" htmlFor="ingredient-search-radio">
           <input
+            className="radio"
             name="radio"
             id="ingredient-search-radio"
             type="radio"
@@ -21,6 +22,7 @@ function SearchDropDown() {
         </label>
         <label className="radio-button" htmlFor="name-search-radio">
           <input
+            className="radio"
             name="radio"
             id="name-search-radio"
             type="radio"
@@ -31,6 +33,7 @@ function SearchDropDown() {
         </label>
         <label className="radio-button" htmlFor="first-letter-search-radio">
           <input
+            className="radio"
             name="radio"
             id="first-letter-search-radio"
             type="radio"

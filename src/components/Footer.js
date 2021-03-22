a5b185bc857bdea69f1788967f8a7a3793e2341f
Fabio Ferreira <fabio35547@gmail.com>
2021-03-22 17:02:33 -0300

    Requisitos concluidos

---
+++ src/components/Footer.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+
+function Footer() {
+  const history = useHistory();
+
+  return (
+    <section className="footer" data-testid="footer">
+      <a
+        data-testid="drinks-bottom-btn"
+        href="/bebidas"
+        src={ drinkIcon }
+      >
+        <img src={ drinkIcon } alt="drink-icon" />
+      </a>
+
+      <button
+        type="button"
+        data-testid="explore-bottom-btn"
+        onClick={ () => history.push('/explorar') }
+        src={ exploreIcon }
+      >
+        <img src={ exploreIcon } alt="explore-icon" />
+      </button>
+
+      <a data-testid="food-bottom-btn" href="/comidas" src={ mealIcon }>
+        <img src={ mealIcon } alt="food icon" />
+      </a>
+    </section>
+  );
+}
+
+export default Footer;

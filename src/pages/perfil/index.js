218c2256a49b9d1489a3bb836529298c6c01c45b
victor Farias <victorfarias.tv@gmail.com>
2021-03-22 17:02:52 -0300

    [Group-4][Req-82-87] - Adicionado tela completa de perfil

--- src/pages/perfil/index.js
+++ src/pages/perfil/index.js
@@ -1,8 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function index() {
+  const logout = () => {
+    localStorage.clear();
+  };
+
+  const userEmail = JSON.parse(localStorage.user).email;
+
   return (
-    <div>OI</div>
+    <div>
+      <h1>Perfil</h1>
+      <p data-testid="profile-email">{ userEmail }</p>
+      <button
+        data-testid="profile-done-btn"
+        type="button"
+      >
+        <Link to="/receitas-feitas">
+          Receitas Feitas
+        </Link>
+      </button>
+      <button
+        data-testid="profile-favorite-btn"
+        type="button"
+      >
+        <Link to="/receitas-favoritas">
+          Receitas Favoritas
+        </Link>
+      </button>
+      <button
+        data-testid="profile-logout-btn"
+        type="button"
+        onClick={ logout }
+      >
+        <Link to="/">
+          Sair
+        </Link>
+      </button>
+    </div>
   );
 }
 

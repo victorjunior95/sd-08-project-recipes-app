cd379f2feba9859ef35c151e57ce69dcd7514513
Luis Felipe Avila <lfelipeavila@outlook.com>
2021-03-22 13:36:33 -0300

    Menu Inferior

--- src/pages/ExplorarComidas.js
+++ src/pages/ExplorarComidas.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Footer from '../components/Footer';
 import HeaderP from '../components/HeaderP';
 
 function ExplorarComidas() {
   return (
-    <HeaderP title="Explorar Comidas" />
+    <>
+      <HeaderP title="Explorar Comidas" />
+      <Footer />
+    </>
   );
 }
 

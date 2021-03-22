7df57e69213fa631fd907100cbef1346248355f9
emassaki <massakihirayama@gmail.com>
2021-03-22 16:49:50 -0300

    Tela de receitas feitas - req 54 ao 59

--- src/pages/Recipes/RecipesDone.js
+++ src/pages/Recipes/RecipesDone.js
@@ -1,8 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
+import copy from 'clipboard-copy';
+import { useHistory } from 'react-router';
+import { Link } from 'react-router-dom';
 import Header from '../../component/Header';
+import shareIcon from '../../images/shareIcon.svg';
 
 export default function RecipesDone() {
+  const storedDone = JSON.parse(localStorage.getItem('doneRecipes'));
+  const [copied, setCopied] = useState(false);
+  const [doneRecipes, setDoneRecipes] = useState(storedDone);
+  const history = useHistory();
+
+  const filterRecipes = (filter) => {
+    const filtered = !filter
+      ? storedDone
+      : storedDone.filter((recipe) => filter === recipe.type);
+    setDoneRecipes(filtered);
+  };
+
+  const copyLink = (eachRecipe) => {
+    copy(`http://localhost:3000/${eachRecipe.type}s/${eachRecipe.id}`);
+    setCopied(true);
+  };
+
   return (
-    <Header pageTitle="Receitas Feitas" showSearchButton={ false } />
+    <>
+      <Header pageTitle="Receitas Feitas" showSearchButton={ false } />
+      <div>
+        <button
+          type="button"
+          data-testid="filter-by-all-btn"
+          onClick={ () => filterRecipes('') }
+        >
+          All
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-food-btn"
+          onClick={ () => filterRecipes('comida') }
+        >
+          Comidas
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-drink-btn"
+          onClick={ () => filterRecipes('bebida') }
+        >
+          Bebidas
+        </button>
+      </div>
+      <div>
+        {doneRecipes.map((eachRecipe, index) => (
+          <div key={ eachRecipe.id }>
+            <Link to={ `/${eachRecipe.type}s/${eachRecipe.id}` }>
+              <img
+                style={ { width: '100vw' } }
+                src={ eachRecipe.image }
+                data-testid={ `${index}-horizontal-image` }
+                alt={ eachRecipe.name }
+              />
+            </Link>
+            <p data-testid={ `${index}-horizontal-top-text` }>
+              {eachRecipe.type === 'comida'
+                ? `${eachRecipe.area} - ${eachRecipe.category}`
+                : `${eachRecipe.alcoholicOrNot}`}
+            </p>
+            <Link
+              to={ `/${eachRecipe.type}s/${eachRecipe.id}` }
+              data-testid={ `${index}-horizontal-name` }
+            >
+              {eachRecipe.name}
+            </Link>
+            <button type="button" onClick={ () => copyLink(eachRecipe) }>
+              <img
+                src={ shareIcon }
+                data-testid={ `${index}-horizontal-share-btn` }
+                alt="Share Button"
+              />
+              {copied && <p>Link copiado!</p>}
+            </button>
+            <p data-testid={ `${index}-horizontal-done-date` }>{eachRecipe.doneDate}</p>
+            {eachRecipe.tags.map((eachTag) => (
+              <span key={ eachTag } data-testid={ `${index}-${eachTag}-horizontal-tag` }>
+                {eachTag}
+              </span>
+            ))}
+          </div>
+        ))}
+      </div>
+    </>
   );
 }

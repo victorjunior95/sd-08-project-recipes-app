3b570f5adb9e79ccaf6cf49bc85c02cedc6e4091
Clênio Galvão <clenio.cgm@gmail.com>
2021-03-22 20:34:31 -0300

    Resolve quesito 15

--- src/pages/Bebidas.js
+++ src/pages/Bebidas.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import ContextRecipes from '../context/ContextRecipes';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-const Bebidas = () => (
-  <section className="w-100">
-    <Header />
-    <h1>
-      Página Bebidas
-    </h1>
-    <Footer />
-  </section>
-);
+const Bebidas = () => {
+  const { setHeaderInfo } = useContext(ContextRecipes);
+  useEffect(() => {
+    setHeaderInfo({ pageTitle: 'Bebidas', showSearch: true });
+  }, [setHeaderInfo]);
+
+  return (
+    <section className="w-100">
+      <Header />
+      <h1>
+        Página Bebidas
+      </h1>
+      <Footer />
+    </section>
+  );
+};
 
 export default Bebidas;

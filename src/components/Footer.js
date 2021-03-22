ee39e96e98c1471e6977a5a73ef6d7b61eb684b4
liisaambrosen <liisa.ambrosen@gmail.com>
2021-03-22 15:45:23 -0300

    req 20

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,16 +2,22 @@ import React from 'react';
 
 function Footer() {
   return (
-    <div data-testid="footer">
-      <button type="button" data-testid="drinks-bottom-btn">
-        Drinks
-      </button>
-      <button type="button" data-testid="explore-bottom-btn">
-        Explorar
-      </button>
-      <button type="button" data-testid="food-bottom-btn">
-        Comidas
-      </button>
+    <div data-testid="footer" className="footer">
+      <img
+        src="/src/images/drinkIcon.svg"
+        data-testid="drinks-bottom-btn"
+        alt="icone drinks"
+      />
+      <img
+        src="/src/images/exploreIcon.svg"
+        data-testid="explore-bottom-btn"
+        alt="icone explore"
+      />
+      <img
+        src="/src/images/mealIcon.svg"
+        data-testid="food-bottom-btn"
+        alt="icone food"
+      />
     </div>
   );
 }

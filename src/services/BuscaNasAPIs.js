db1259e9021471bf4a3ff7e6f5e1be2617a58a9e
Igor <igorln96@gmail.com>
2021-03-22 19:52:50 -0300

    fix eslint

--- src/services/BuscaNasAPIs.js
+++ src/services/BuscaNasAPIs.js
@@ -1,6 +1,6 @@
 export const getComidasRandom = () => (
   fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-  .then((response) => response.json())
+    .then((response) => response.json())
     .then((json) => json)
     .catch((error) => console.log(error))
 );

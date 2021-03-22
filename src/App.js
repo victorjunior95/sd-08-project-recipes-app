fd020e791c2e09bcabc275c12f69168cec2bdaf9
liisaambrosen <liisa.ambrosen@gmail.com>
2021-03-22 16:20:01 -0300

    requisitos 22, 23, 24

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Comidas from './pages/Comidas';
+import Bebidas from './pages/Bebidas';
+import Explorar from './pages/Explorar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
     <Switch>
       <Route exact path="/" component={ Login } />
       <Route path="/comidas" component={ Comidas } />
+      <Route path="/bebidas" component={ Bebidas } />
+      <Route path="/explorar" component={ Explorar } />
     </Switch>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,32 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 function Footer() {
+  const history = useHistory();
+
   return (
     <div data-testid="footer" className="footer">
-      <img
-        src="/src/images/drinkIcon.svg"
-        data-testid="drinks-bottom-btn"
-        alt="icone drinks"
-      />
-      <img
-        src="/src/images/exploreIcon.svg"
-        data-testid="explore-bottom-btn"
-        alt="icone explore"
-      />
-      <img
-        src="/src/images/mealIcon.svg"
-        data-testid="food-bottom-btn"
-        alt="icone food"
-      />
+      <button type="button" onClick={ () => history.push('/bebidas') }>
+        <img
+          src="../images/drinkIcon.svg"
+          data-testid="drinks-bottom-btn"
+          alt="icone drinks"
+        />
+      </button>
+      <button type="button" onClick={ () => history.push('/explorar') }>
+        <img
+          src="../images/exploreIcon.svg"
+          data-testid="explore-bottom-btn"
+          alt="icone explore"
+        />
+      </button>
+      <button type="button" onClick={ () => history.push('/comidas') }>
+        <img
+          src="../images/mealIcon.svg"
+          data-testid="food-bottom-btn"
+          alt="icone food"
+        />
+      </button>
     </div>
   );
 }

6f2886cf2ec8259a71772dd91fdadd606a5b91cb
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2021-03-22 17:04:20 -0300

    requisito 6 7 e 8

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,17 @@
 import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 import Input from '../components/Input';
 import Button from '../components/Button';
+import {
+  setMealsToken,
+  setCocktailsToken,
+  setUser,
+} from '../services/setLocalStorage';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [shouldRedirect, setShouldRedirect] = useState(false);
 
   const checkEmailAndPassword = () => {
     const minimumPasswordSize = 6;
@@ -25,28 +32,42 @@ const Login = () => {
     setPassword(value);
   };
 
+  const onClick = () => {
+    setMealsToken();
+    setCocktailsToken();
+    setUser(email);
+    setShouldRedirect(true);
+  };
+
   return (
-    <form>
-      <Input
-        name="email"
-        type="email"
-        placeholder="Email"
-        data-testid="email-input"
-        onChange={ emailChange }
-      />
-      <Input
-        name="password"
-        type="password"
-        placeholder="Senha"
-        data-testid="password-input"
-        onChange={ passwordChange }
-      />
-      <Button
-        name="Entrar"
-        data-testid="login-submit-btn"
-        disabled={ checkEmailAndPassword() }
-      />
-    </form>
+    <div>
+      {shouldRedirect ? (
+        <Redirect to="/comidas" />
+      ) : (
+        <form>
+          <Input
+            name="email"
+            type="email"
+            placeholder="Email"
+            data-testid="email-input"
+            onChange={ emailChange }
+          />
+          <Input
+            name="password"
+            type="password"
+            placeholder="Senha"
+            data-testid="password-input"
+            onChange={ passwordChange }
+          />
+          <Button
+            name="Entrar"
+            data-testid="login-submit-btn"
+            disabled={ checkEmailAndPassword() }
+            onClick={ onClick }
+          />
+        </form>
+      )}
+    </div>
   );
 };
 

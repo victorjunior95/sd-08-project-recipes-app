2c343ee58f773a7c576c62cc8d5c41154ec74675
Lucas Rodrigues <lucas.movimento@gmail.com>
2021-03-22 00:09:36 -0300

    inicia estrutura de tests e conclui teste de Login

--- src/_tests/App.test.js
+++ src/_tests/App.test.js
@@ -4,7 +4,27 @@ import userEvent from '@testing-library/user-event';
 import renderWithReduxandRouter from './renderWithReduxandRouter';
 import App from '../App';
 
-describe('App.js', () => {
+let localStorageMock = {};
+
+beforeAll(() => {
+  global.Storage.prototype.setItem = jest.fn((key, value) => {
+    localStorageMock[key] = value;
+  });
+  global.Storage.prototype.getItem = jest.fn((key) => localStorageMock[key]);
+});
+
+beforeEach(() => {
+  localStorageMock = {};
+});
+
+afterAll(() => {
+  global.Storage.prototype.setItem.mockReset();
+  global.Storage.prototype.getItem.mockReset();
+});
+
+const EMAIL_INPUT = 'email@mail.com';
+
+describe('LoginPage.js', () => {
   test('if all the Routes are working', () => {
     const { history } = renderWithReduxandRouter(<App />);
 
@@ -21,5 +41,43 @@ describe('App.js', () => {
     expect(emailInput).toBeInTheDocument();
     expect(passwordInput).toBeInTheDocument();
     expect(buttonEnter).toBeInTheDocument();
+  });
+
+  test('if Login receive correct inputs from user', () => {
+    renderWithReduxandRouter(<App />);
+
+    const emailInput = screen.getByLabelText(/email/i);
+    const passwordInput = screen.getByLabelText(/password/i);
+    const buttonEnter = screen.getByRole('button', { name: /entrar/i }).closest('button');
+
+    expect(buttonEnter).toBeDisabled();
+
+    userEvent.type(emailInput, EMAIL_INPUT);
+    userEvent.type(passwordInput, '1234567');
+
+    expect(emailInput).toHaveValue(EMAIL_INPUT);
+    expect(passwordInput).toHaveValue('1234567');
+    expect(buttonEnter).not.toBeDisabled();
+  });
+
+  test('if localStorage is set as the enter button is clicked', () => {
+    const { history } = renderWithReduxandRouter(<App />);
+
+    const emailInput = screen.getByLabelText(/email/i);
+    const passwordInput = screen.getByLabelText(/password/i);
+    const buttonEnter = screen.getByRole('button', { name: /entrar/i }).closest('button');
+
+    userEvent.type(emailInput, EMAIL_INPUT);
+    userEvent.type(passwordInput, '1234567');
+    userEvent.click(buttonEnter);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/comidas');
+
+    const userToken = JSON.stringify({ email: EMAIL_INPUT });
+
+    expect(localStorageMock.mealsToken).toEqual(1);
+    expect(localStorageMock.cocktailsToken).toEqual(1);
+    expect(localStorageMock.user).toEqual(userToken);
   });
 });

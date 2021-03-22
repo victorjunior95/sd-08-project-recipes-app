87e6dd830507d5ab546f91f7b034ffbfdb607e3f
Blucilha <blucilha.nascimento@gmail.com>
2021-03-22 17:18:45 -0300

    Requisito do 01 ao 08

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React from 'react';
+import RecipesProvider from './ContextApi/RecipesProvider';
+import RecipesFood from './pages/RecipesFood';
+import Login from './pages/Login';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+// import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    //gif inicial da Trybe
+    // <div className="meals">
+    //   <span className="logo">TRYBE</span>
+    //   <object
+    //     className="rocksGlass"
+    //     type="image/svg+xml"
+    //     data={ rockGlass }
+    //   >
+    //     Glass
+    //   </object>
+    // </div>
+    <RecipesProvider>
+      <Switch>
+        <Route exact path='/' component={ Login } />
+        <Route path='/comidas' component={ RecipesFood } />
+      </Switch>
+    </RecipesProvider>
   );
 }
 
---
+++ src/ContextApi/RecipesProvider.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import RecipesContext from './RecipesContext';
+
+function RecipesProvider({ children }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmail = ({ target: { value } }) => {
+    setEmail(value);
+  }
+  
+  const handlePassword = ({ target: { value } }) => {
+    setPassword(value);
+  }
+  
+  const provide = {
+    email,
+    password,
+    handleEmail,
+    handlePassword,
+  }
+
+  return (
+    <RecipesContext.Provider value={ provide }>
+      { children }
+    </RecipesContext.Provider>
+  )
+}
+
+RecipesProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default RecipesProvider;

2f62bfb09e528a836f376c210d146e8d6d8d11cb
Rafael <rafaeljavaro750@gmail.com>
2021-03-22 17:51:40 -0300

    Arruma questao do Lint, Reqs 01 ao 08

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import RecipesProvider from './ContextApi/RecipesProvider';
 import RecipesFood from './pages/RecipesFood';
 import Login from './pages/Login';
-import { Route, Switch } from 'react-router-dom';
 import './App.css';
 // import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    //gif inicial da Trybe
+    // gif inicial da Trybe
     // <div className="meals">
     //   <span className="logo">TRYBE</span>
     //   <object
@@ -22,8 +22,8 @@ function App() {
     // </div>
     <RecipesProvider>
       <Switch>
-        <Route exact path='/' component={ Login } />
-        <Route path='/comidas' component={ RecipesFood } />
+        <Route exact path="/" component={ Login } />
+        <Route path="/comidas" component={ RecipesFood } />
       </Switch>
     </RecipesProvider>
   );

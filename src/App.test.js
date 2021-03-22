a0d2010e1e56ae164dfac03ddc8accd379724995
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-03-22 13:34:30 -0300

    cria os testes do componente de login

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,32 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
-test('Farewell, front-end', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/TRYBE/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Testa a tela de Login', () => {
+  it('verifica existencia dos inputs e do botão', () => {
+    const { getByTestId } = renderWithRouter(<App />);
+    const emailInput = getByTestId('email-input');
+    const passwordInput = getByTestId('password-input');
+    const loginButton = getByTestId('login-submit-btn');
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(loginButton).toBeInTheDocument();
+  });
+
+  it('verifica se o login executa corretamente', () => {
+    const { getByTestId, history } = renderWithRouter(<App />);
+    const userEmail = 'teste@teste.com';
+    const userPassword = '123456789';
+    const emailInput = getByTestId('email-input');
+    const passwordInput = getByTestId('password-input');
+    const loginButton = getByTestId('login-submit-btn');
+    userEvent.type(emailInput, userEmail);
+    userEvent.type(passwordInput, userPassword);
+    userEvent.click(loginButton);
+    const pathname = history.location.pathname;
+    expect(pathname).toBe('/comidas');
+  });
+})
+
+

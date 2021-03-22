a04ee7ce143025cbcba281470f6aed58352eaa49
VitorLimaRios <vitorlimarios999@gmail.com>
2021-03-22 18:42:52 -0300

    Corrigindo conflitos

--- src/pages/bebida/index.js
+++ src/pages/bebida/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Header from '../../components/Header';
-import SearchBarDrink from '../../components/SearchBarDrink';
 import CardDrink from '../../components/CardDrink';
 
 function Bebida() {
   return (
     <section>
       <Header>Bebidas</Header>
-      <SearchBarDrink />
       <CardDrink />
     </section>
   );
--- src/pages/comida/index.js
+++ src/pages/comida/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Header from '../../components/Header';
-import SearchBarFood from '../../components/SearchBarFood';
 import CardFood from '../../components/CardFood';
 
 function Comida() {
   return (
     <section>
       <Header>Comidas</Header>
-      <SearchBarFood />
       <CardFood />
     </section>
   );

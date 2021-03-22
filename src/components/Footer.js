1fbcea320f7071311dcceebef0d438e208d2b5a3
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-03-22 17:17:44 -0300

    Adiciona requisitos 19 e 20

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import DrinkIcon from '../images/drinkIcon.svg';
+import ExploreIcon from '../images/exploreIcon.svg';
+import MealIcon from '../images/mealIcon.svg';
 
 function Footer() {
-
+  return (
+    <footer data-testid="footer" className="g6-footer">
+      <button
+        type="button"
+        data-testid="drinks-bottom-btn"
+        onClick=""
+        className="g6-footer-button"
+      >
+        <img src={ DrinkIcon } alt="Bebidas" />
+      </button>
+      <button
+        type="button"
+        data-testid="explore-bottom-btn"
+        onClick=""
+        className="g6-footer-button"
+      >
+        <img src={ ExploreIcon } alt="Explorar" />
+      </button>
+      <button
+        type="button"
+        data-testid="food-bottom-btn"
+        onClick=""
+        className="g6-footer-button"
+      >
+        <img src={ MealIcon } alt="Comidas" />
+      </button>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/pages/Comidas.js
+++ src/pages/Comidas.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function Comidas() {
   return (
     <div>
       <Header pageTitle="Comidas" />
+      <Footer />
     </div>
   );
 }

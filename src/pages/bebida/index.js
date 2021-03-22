b69629d18c580736925f801e50bcf2159f8de852
nyxnil <nsmnilsonn@gmail.com>
2021-03-22 17:07:21 -0300

    footer iniciado requisitos 19 ao 24

--- src/pages/bebida/index.js
+++ src/pages/bebida/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Footer from '../../components/footer';
+
 
 function index() {
   return (
-    <div>OI</div>
-  );
+    <div>
+      OI
+      <Footer />
+    </div>
+    );
 }
 
 export default index;

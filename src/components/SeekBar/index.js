dee530bfbeb3bfe47241982aa04fe5f32068befc
UmberBn <humberto-bandeira@hotmail.com>
2021-03-22 20:33:56 -0400

    Atualizaçoes da barra de pesquisa

--- src/components/SeekBar/index.js
+++ src/components/SeekBar/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Form, FormCheck, FormControl, Button } from 'react-bootstrap';
+import { Form, FormControl, Button, Row } from 'react-bootstrap';
 
 export default function SeekBar() {
   const [onSeek, setOnSeek] = useState('');
@@ -7,32 +7,44 @@ export default function SeekBar() {
   return (
 
     <Form>
-      <FormControl
-        type="text"
-        placeholder="Pesquisar"
-        className="mt-3"
-        data-testid="search-input"
-        onChange={ (e) => setOnSeek(e.target.value) }
-      />
-      <Form.Group>
-        <FormCheck
+      <Form.Row>
+        <FormControl
+          type="text"
+          placeholder="Pesquisar"
+          className="m-3"
+          data-testid="search-input"
+          onChange={ (e) => setOnSeek(e.target.value) }
+        />
+      </Form.Row>
+      <Form.Group as={ Row } className="justify-content-center">
+        <Form.Check
+          sm={ 4 }
           data-testid="ingredient-search-radio"
           label="Ingrediente"
           type="radio"
+          name="SearchChoise"
+          className="mr-3"
         />
-        <FormCheck
+        <Form.Check
+          sm={ 4 }
           data-testid="name-search-radio"
           label="Nome"
           type="radio"
+          name="SearchChoise"
+          className="mr-3"
         />
-        <FormCheck
+        <Form.Check
+          sm={ 4 }
           data-testid="first-letter-search-radio"
           label="Primeira Letra"
           type="radio"
+          name="SearchChoise"
+          className="mr-3"
         />
       </Form.Group>
       <Button
         data-testid="exec-search-btn"
+        className="ml-3"
       >
         Buscar
       </Button>

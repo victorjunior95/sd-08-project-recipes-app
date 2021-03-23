32757139c4ca4c6eb988710e2548fbe7ce9b77e4
Isabella Paz <bellapaz.iorp@gmail.com>
2021-03-23 16:35:03 -0300

    index das paginas

---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+export { default as NotFound } from './NotFound';
+export { default as Login } from './Login';
+export { default as Comidas } from './Comidas';
+export { default as Bebidas } from './Bebidas';
+export { default as DetalhesReceita } from './DetalhesReceita';
+export { default as Explorar } from './Explorar';
+export { default as ExplorarIngredientes } from './ExplorarIngredientes';
+export { default as ExplorarBebidaOuComida } from './ExplorarBebidaOuComida';
+export { default as ExplorarPorLocalDeOrigem } from './ExplorarPorLocalDeOrigem';
+export { default as Perfil } from './Perfil';
+export { default as ReceitasFeitas } from './ReceitasFeitas';
+export { default as ReceitasFavoritas } from './ReceitasFavoritas';
+export { default as ReceitaEmProgresso } from './ReceitaEmProgresso';

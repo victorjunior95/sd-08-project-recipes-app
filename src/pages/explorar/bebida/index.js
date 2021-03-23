5227a48da1d54b3bd2d4e097a46065efd24bc1a7
Rosiele David <rosieleserpa@hotmail.com>
2021-03-23 17:13:28 -0300

    [Group-4][Requ70-73] - criaçao dos botoes na tela de explorar

--- src/pages/explorar/bebida/index.js
+++ src/pages/explorar/bebida/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../../../components/Header';
 
 function ExplorarBebida() {
   return (
-    <div>
+    <>
       <Header explore="false">Explorar Bebidas</Header>
-    </div>
+      <section>
+        <Link to="/explorar/bebidas/ingredientes">
+          <button type="button" data-testid="explore-by-ingredient">
+            Por Ingredientes
+          </button>
+        </Link>
+        <button type="button" data-testid="explore-surprise">
+          Me Surpreenda!
+        </button>
+      </section>
+    </>
   );
 }
 
--- src/pages/explorar/comida/index.js
+++ src/pages/explorar/comida/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../../../components/Header';
 
 function ExplorarComida() {
   return (
-    <div>
+    <>
       <Header explore="false">Explorar Comidas</Header>
-    </div>
+      <section>
+        <Link to="/explorar/comidas/ingredientes">
+          <button type="button" data-testid="explore-by-ingredient">
+            Por Ingredientes
+          </button>
+        </Link>
+        <Link to="/explorar/comidas/area">
+          <button type="button" data-testid="explore-by-area">
+            Por Local de Origem
+          </button>
+        </Link>
+        <button type="button" data-testid="explore-surprise">
+          Me Surpreenda!
+        </button>
+      </section>
+    </>
   );
 }
 

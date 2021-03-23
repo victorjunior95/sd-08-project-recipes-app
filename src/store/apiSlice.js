2fcbfaf088eef8587e3e31606294d8a229e7ef37
Silvio Dayube <scarige@gmail.com>
2021-03-23 17:38:02 -0300

    Grupo 20 - Requisito 14

---
+++ src/store/apiSlice.js
@@ -0,0 +1,28 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+export const fetchItem = createAsyncThunk(
+  'api/fetchItem',
+  async (endPoint) => {
+    const data = await fetch(endPoint)
+      .then((response) => response.json());
+    return data;
+  },
+);
+
+const apiSlice = createSlice({
+  name: 'api',
+  initialState: {
+    data: [],
+    loading: 'idle',
+  },
+  reducers: {},
+  extraReducers: {
+    [fetchItem.pending]: (state) => ({ ...state, loading: 'pending' }),
+    [fetchItem.fulfilled]: (state, action) => ({
+      data: action.payload, loading: 'fulfilled',
+    }),
+    [fetchItem.rejected]: (state) => ({ ...state, loading: 'rejected' }),
+  },
+});
+
+export default apiSlice.reducer;
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import search from './searchSlice';
 import login from './loginSlice';
+import api from './apiSlice';
 
 export default configureStore({
   reducer: {
     search,
     login,
+    api,
   },
 });

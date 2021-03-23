1d0829a48ff55e1e456930d15b3577bc37b94351
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-03-23 09:24:43 -0300

    Requisitos 67-69

--- src/pages/Explorar.js
+++ src/pages/Explorar.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function Explorar() {
+  const history = useHistory();
+
+  const handleClick = (e) => {
+    history.push(`/explorar/${e.target.name}`);
+  };
+
   return (
     <div>
       <Header pageTitle="Explorar" />
+      <div className="g6-explore">
+        <button
+          type="button"
+          data-testid="explore-food"
+          name="comidas"
+          onClick={ handleClick }
+        >
+          Explorar Comidas
+        </button>
+        <button
+          type="button"
+          data-testid="explore-drinks"
+          name="bebidas"
+          onClick={ handleClick }
+        >
+          Explorar Bebidas
+        </button>
+      </div>
       <Footer />
     </div>
   );
---
+++ src/pages/ExplorarComidas.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+
+function ExplorarComidas() {
+  return (
+    <div>
+      <Header pageTitle="Explorar Comidas" />
+      <Footer />
+    </div>
+  );
+}
+
+export default ExplorarComidas;

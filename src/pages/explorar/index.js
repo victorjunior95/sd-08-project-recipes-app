dc3dc81d0a6ff5be92f97fca5c071f72893d7dcd
nyxnil <nsmnilsonn@gmail.com>
2021-03-23 12:08:44 -0300

    footer requisitos 19 a 24

--- src/pages/explorar/index.js
+++ src/pages/explorar/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from '../../components/footer';
 import Header from '../../components/Header';
 
 function Explorar() {
   return (
     <div>
       <Header explore="false">Explorar</Header>
+      <Footer />
     </div>
   );
 }
--- src/pages/perfil/index.js
+++ src/pages/perfil/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Footer from '../../components/footer';
 import Header from '../../components/Header';
 
 function Perfil() {
@@ -37,6 +38,7 @@ function Perfil() {
           Sair
         </Link>
       </button>
+      <Footer />
     </div>
   );
 }

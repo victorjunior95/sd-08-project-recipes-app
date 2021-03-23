8f6c28e96d88dddfbcd4c43f2e386de7e1cf2b21
Isabella Paz <bellapaz.iorp@gmail.com>
2021-03-23 16:36:04 -0300

    arquivo da page ExplorarPorLocalDeOrigem

---
+++ src/pages/ExplorarPorLocalDeOrigem.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from '../components/Footer';
+
+function ExplorarPorLocalDeOrigem() {
+  return (
+    <div>
+      <h1>Explorar por local de origem</h1>
+      <Footer />
+    </div>
+  );
+}
+
+export default ExplorarPorLocalDeOrigem;

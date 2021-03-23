0fe690655b187bd602de5abde9994fdfe6262547
Rosiele David <rosieleserpa@hotmail.com>
2021-03-23 16:05:35 -0300

    [Group-4][Requ67-69] - criacao da tela de explorar

--- src/pages/explorar/index.js
+++ src/pages/explorar/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
 
 function Explorar() {
   return (
-    <div>
+    <section>
       <Header explore="false">Explorar</Header>
-    </div>
+      <Link to="/explorar/comidas">
+        <button type="button" data-testid="explore-food">
+          Explorar Comidas
+        </button>
+      </Link>
+      <Link to="/explorar/bebidas" data-testid="explore-drinks">
+        <button type="button">
+          Explorar Bebidas
+        </button>
+      </Link>
+    </section>
   );
 }
 

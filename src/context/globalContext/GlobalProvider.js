b9d5fc9db4660455bdd23c1cfba0c2128e9f20b2
VitorLimaRios <vitorlimarios999@gmail.com>
2021-03-23 16:36:43 -0300

    [Group-4] - Adicionando Contexto Global

---
+++ src/context/globalContext/GlobalProvider.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import GlobalContext from './GlobalContext';
+
+function GlobalProvider({ children }) {
+  const [email, setEmail] = useState('');
+
+  const provide = {
+    values: {
+      email,
+    },
+    functions: {
+      setEmail,
+    },
+  };
+  return (
+    <GlobalContext.Provider value={ provide }>
+      {children}
+    </GlobalContext.Provider>
+  );
+}
+
+GlobalProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default GlobalProvider;

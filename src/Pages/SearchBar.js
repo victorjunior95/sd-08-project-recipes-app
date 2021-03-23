6a7ae4e8257f3c4a0c1bf6ec766646d3939f3088
Silvio Dayube <scarige@gmail.com>
2021-03-22 23:04:05 -0300

    Grupo 20 - Requisito 13

--- src/Pages/SearchBar.js
+++ src/Pages/SearchBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { Input } from '../Components';
 
@@ -9,6 +9,28 @@ function SearchBar() {
     return (
       <Form>
         <Input name="Pesquisar" dataId="search-input" />
+        <Form.Check
+          type="radio"
+          id="ingredient-search-radio"
+          data-testid="ingredient-search-radio"
+          label="Ingredientes"
+          inline
+        />
+        <Form.Check
+          type="radio"
+          id="name-search-radio"
+          data-testid="name-search-radio"
+          label="Nome"
+          inline
+        />
+        <Form.Check
+          type="radio"
+          id="first-letter-search-radio"
+          data-testid="first-letter-search-radio"
+          label="Primeira Letra"
+          inline
+        />
+        <Button variant="primary" data-testid="exec-search-btn">Pesquisar</Button>
       </Form>
     );
   }

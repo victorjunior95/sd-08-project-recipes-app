b38da11c874ac2f19cdcced25d527b0e2e06b277
rodolfo_oliveira <rod.vgo@gmail.com>
2021-03-23 20:34:53 -0300

    ajustes

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,12 +6,12 @@ import SearchIcon from '../images/searchIcon.svg';
 export default function Header() {
   return (
     <header>
-      <button type="button" data-testid="search-top-btn">
-        <img src={ SearchIcon } alt="search-icon" />
+      <button type="button">
+        <img src={ SearchIcon } alt="search-icon" data-testid="search-top-btn" />
       </button>
       <h1 data-testid="page-title">Comidas</h1>
-      <button type="button" data-testid="profile-top-btn">
-        <img src={ ProfileIcon } alt="profile-icon" />
+      <button type="button">
+        <img src={ ProfileIcon } alt="profile-icon" data-testid="profile-top-btn" />
       </button>
     </header>
   );

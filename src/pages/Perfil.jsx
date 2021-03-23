b43555a629ad1ccac7aa861a16b20974c1018de7
Isabella Paz <bellapaz.iorp@gmail.com>
2021-03-23 16:58:05 -0300

    adicionando o footer na page perfil

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Footer from '../components/Footer';
 
 function Perfil() {
   return (
     <div>
       <h1>Perfil... página em construção</h1>
+      <Footer />
     </div>
   );
 }

f720f33f33fc220314897981a6eafeeed5267399
UmberBn <humberto-bandeira@hotmail.com>
2021-03-23 18:18:21 -0400

    Requisitos de 70 a 73

--- src/pages/ExploreFoods/index.js
+++ src/pages/ExploreFoods/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button, NavbarBrand } from 'react-bootstrap';
 import ContainerDefault from '../../components/ContainerDefault';
 
 // import { Container } from './styles';
@@ -6,7 +7,36 @@ import ContainerDefault from '../../components/ContainerDefault';
 function ExploreFoods() {
   return (
     <ContainerDefault title="Explorar Comidas">
-      <p>Explorar: Comidas</p>
+      <NavbarBrand href="/explorar/comidas/ingredientes" className="mx-0">
+        <Button
+          type="button"
+          className="mb-3"
+          data-testid="explore-by-ingredient"
+          size="block"
+        >
+          Por Ingredientes
+        </Button>
+      </NavbarBrand>
+      <NavbarBrand href="/explorar/comidas/area" className="mx-0">
+        <Button
+          type="button"
+          className="mb-3"
+          data-testid="explore-by-area"
+          size="block"
+        >
+          Por Local de Origem
+        </Button>
+      </NavbarBrand>
+      <NavbarBrand href="/explorar/comidas/area" className="mx-0">
+        <Button
+          type="button"
+          className="mb-3"
+          data-testid="explore-surprise"
+          size="block"
+        >
+          Me Surpreenda!
+        </Button>
+      </NavbarBrand>
     </ContainerDefault>
   );
 }

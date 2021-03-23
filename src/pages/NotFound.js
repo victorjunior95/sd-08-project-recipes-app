af696cadf629f1adff822651752da8490f20e812
Ediberto Oliveira <edibertooliveira@aol.com>
2021-03-23 13:13:05 -0300

    fix:  clear commit

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,14 +1,9 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
 
 class NotFound extends Component {
   render() {
     return <div>404</div>;
   }
 }
-
-// Drinks.propTypes = {
-
-// };
 
 export default NotFound;

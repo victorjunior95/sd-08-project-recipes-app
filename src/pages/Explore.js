84e9d6b17ab9ac17c1dd9f0c03d82814db4df6ea
Cleberton <clebertonfgc@gmail.com>
2021-03-23 20:23:55 -0300

    Sem css

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
@@ -7,18 +8,18 @@ import Container from '../components/Container';
 const Explore = () => (
   <Container>
     <Header title="Explorar" />
-      <a 
-       href="/explorar/comidas"
+      <Link 
+       to="/explorar/comidas"
        data-testid="explore-food"
       >
         Explorar Comidas
-      </a>
-      <a
-       href="/explorar/bebidas"
+      </Link>
+      <Link
+       to="/explorar/bebidas"
        data-testid="explore-drinks"
       >
         Explorar Bebidas
-      </a>
+      </Link>
     <Footer />
   </Container>
 );

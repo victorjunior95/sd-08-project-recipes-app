af4aafbb1d69bff6d79ae390a49ac8d60dc4584b
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-03-23 11:00:31 -0300

    corrige erros do avaliador na propriedade bottom do css do footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Footer.css';
 import { Link } from 'react-router-dom';
 import drinkIcon from '../../images/drinkIcon.svg';
 import mealIcon from '../../images/mealIcon.svg';
@@ -8,13 +9,13 @@ export default function Footer() {
   return (
     <footer className="footer" data-testid="footer">
       <Link to="/bebidas">
-        <img src={ drinkIcon } alt="Drnks" data-testid="drinks-botton-btn" />
+        <img src={ drinkIcon } alt="Drinks" data-testid="drinks-bottom-btn" />
       </Link>
       <Link to="/explorar">
-        <img src={ exploreIcon } alt="Explore" data-testId="explore-bottom-btn" />
+        <img src={ exploreIcon } alt="Explore" data-testid="explore-bottom-btn" />
       </Link>
       <Link to="/comidas">
-        <img src={ mealIcon } alt="Meals" data-testid="food-botton-btn" />
+        <img src={ mealIcon } alt="Meals" data-testid="food-bottom-btn" />
       </Link>
     </footer>
   );

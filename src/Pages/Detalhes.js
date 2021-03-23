447600e48d8941d4dddb26b291aad649fbfc48d4
Douglas Ferreira <dodofp.df@gmail.com>
2021-03-23 18:14:29 -0300

    ESlint styles

--- src/Pages/Detalhes.js
+++ src/Pages/Detalhes.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import RecipeContext from '../context/RecipeContext';
 import '../styles/Card.css';
+// import shareIcon from '../images/shareIcon.svg'
 
 function Detalhes() {
   const { setShouldRedirect } = useContext(RecipeContext);
@@ -13,16 +14,20 @@ function Detalhes() {
     const value = history.location.pathname;
     let response = [];
     const id = value.split('s/')[1];
-    if (value.includes('comidas')) {
-      response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
-      const responseJson = await response.json();
-      setObjDetail(responseJson.meals);
-    }
     if (value.includes('bebidas')) {
       response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
       const responseJson = await response.json();
-      setObjDetail(responseJson.drinks);
+      await setObjDetail(responseJson.drinks);
     }
+    if (value.includes('comidas')) {
+      response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+      const responseJson = await response.json();
+      await setObjDetail(responseJson.meals);
+    }
+
+    // setTimeout(() => {
+    // setLoading(false);
+    // }, 2000)
     setLoading(false);
   };
 
@@ -33,20 +38,23 @@ function Detalhes() {
 
   const renderDrink = () => (
     <div>
-      {console.log(objDetail[0])}
-      <h1>{objDetail[0].strDrink}</h1>
+      <h1 data-testid="recipe-title">{objDetail[0].strDrink}</h1>
       <img
+        data-testid="recipe-photo"
         className="food-image"
         src={ objDetail[0].strDrinkThumb }
         alt={ objDetail[0].strDrink }
       />
+      {/* <img data-testid="share-btn" />
+      <input type="image" src={ shareIcon } /> */}
     </div>
   );
 
   const renderFood = () => (
     <div>
-      <h1>{objDetail[0].strMeal}</h1>
+      <h1 data-testid="recipe-title">{objDetail[0].strMeal}</h1>
       <img
+        data-testid="recipe-photo"
         className="food-image"
         src={ objDetail[0].strMealThumb }
         alt={ objDetail[0].strMeal }
@@ -67,7 +75,6 @@ function Detalhes() {
 
   return (
     <div>
-      {console.log(objDetail[0])}
       {loading ? <p>Carregando</p> : render()}
     </div>
   );

f96c55e36173dd3d0080925b086d298433789c5b
Blucilha <blucilha.nascimento@gmail.com>
2021-03-23 15:33:16 -0300

    Testes do App

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
-=======
 
->>>>>>> 2e491ee2226ed424a7aa2e435181914404a525b6
 import ProfileIcon from '../images/profileIcon.svg';
 import SearchIcon from '../images/searchIcon.svg';
 

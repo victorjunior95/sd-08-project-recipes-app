147f8d3d615d91b31d6fc5ff59d09888e2bebd4f
Isabella Paz <bellapaz.iorp@gmail.com>
2021-03-23 18:54:09 -0300

    alteracao das rotas das paginas ExplorarComidas e ExplorarBebidas

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Switch, Route } from 'react-router-dom';
+import ProviderReceitas from './context/ProviderReceitas';
 import {
   Login,
   Comidas,
   Bebidas,
   NotFound,
   DetalhesReceita,
-  Explorar,
-  ExplorarIngredientes,
   ExplorarBebidaOuComida,
+  ExplorarComidas,
+  ExplorarBebidas,
+  ExplorarIngredientes,
   ExplorarPorLocalDeOrigem,
   Perfil,
   ReceitasFeitas,
@@ -21,42 +23,44 @@ import {
 function App() {
   return (
     <div className="main">
-      <Switch>
-        <Route exact path="/comidas" component={ Comidas } />
-        <Route exact path="/comidas/:id/in-progress" component={ ReceitaEmProgresso } />
-        <Route exact path="/comidas/:id" component={ DetalhesReceita } />
-        <Route exact path="/bebidas" component={ Bebidas } />
-        <Route exact path="/bebidas/:id/in-progress" component={ ReceitaEmProgresso } />
-        <Route exact path="/bebidas/:id" component={ DetalhesReceita } />
-        <Route exact path="/explorar" component={ ExplorarBebidaOuComida } />
-        <Route exact path="/explorar/comidas" component={ Explorar } />
-        <Route exact path="/explorar/bebidas" component={ Explorar } />
-        <Route
-          exact
-          path="/explorar/comidas/ingredientes"
-          component={ ExplorarIngredientes }
-        />
-        <Route
-          exact
-          path="/explorar/bebidas/ingredientes"
-          component={ ExplorarIngredientes }
-        />
-        <Route
-          exact
-          path="/explorar/comidas/area"
-          component={ ExplorarPorLocalDeOrigem }
-        />
-        <Route exact path="/perfil" component={ Perfil } />
-        <Route exact path="/receitas-feitas" component={ ReceitasFeitas } />
-        <Route exact path="/receitas-favoritas" component={ ReceitasFavoritas } />
-        <Route
-          exact
-          path="/explorar/bebidas/area"
-          component={ ExplorarPorLocalDeOrigem }
-        />
-        <Route exact path="/" component={ Login } />
-        <Route exact path="*" component={ NotFound } />
-      </Switch>
+      <ProviderReceitas>
+        <Switch>
+          <Route exact path="/comidas" component={ Comidas } />
+          <Route exact path="/comidas/:id/in-progress" component={ ReceitaEmProgresso } />
+          <Route exact path="/comidas/:id" component={ DetalhesReceita } />
+          <Route exact path="/bebidas" component={ Bebidas } />
+          <Route exact path="/bebidas/:id/in-progress" component={ ReceitaEmProgresso } />
+          <Route exact path="/bebidas/:id" component={ DetalhesReceita } />
+          <Route exact path="/explorar" component={ ExplorarBebidaOuComida } />
+          <Route exact path="/explorar/comidas" component={ ExplorarComidas } />
+          <Route exact path="/explorar/bebidas" component={ ExplorarBebidas } />
+          <Route
+            exact
+            path="/explorar/comidas/ingredientes"
+            component={ ExplorarIngredientes }
+          />
+          <Route
+            exact
+            path="/explorar/bebidas/ingredientes"
+            component={ ExplorarIngredientes }
+          />
+          <Route
+            exact
+            path="/explorar/comidas/area"
+            component={ ExplorarPorLocalDeOrigem }
+          />
+          <Route exact path="/perfil" component={ Perfil } />
+          <Route exact path="/receitas-feitas" component={ ReceitasFeitas } />
+          <Route exact path="/receitas-favoritas" component={ ReceitasFavoritas } />
+          <Route
+            exact
+            path="/explorar/bebidas/area"
+            component={ ExplorarPorLocalDeOrigem }
+          />
+          <Route exact path="/" component={ Login } />
+          <Route exact path="*" component={ NotFound } />
+        </Switch>
+      </ProviderReceitas>
     </div>
   );
 }
--- src/context/ProviderReceitas.js
+++ src/context/ProviderReceitas.js
@@ -4,14 +4,25 @@ import ContextReceitas from './ContextReceitas';
 
 function ProviderReceitas({ children }) {
   const [logedIn, setLoged] = useState(0);
+  const [tituloDaPagina, enviarTituloDaPagina] = useState('Comidas');
+  const [statusBotaoPesquisa, mudarStatusBotaoPesquisa] = useState(true);
+  const dados = {
+    logedIn,
+    setLoged,
+    tituloDaPagina,
+    enviarTituloDaPagina,
+    statusBotaoPesquisa,
+    mudarStatusBotaoPesquisa,
+  };
   return (
-    <ContextReceitas.Provider value={ { logedIn, setLoged } }>
+    <ContextReceitas.Provider value={ dados }>
       { children }
     </ContextReceitas.Provider>
   );
 }
 
 export default ProviderReceitas;
+
 ProviderReceitas.propTypes = {
   children: PropTypes.element.isRequired,
 };

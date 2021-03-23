6200d14e891d7e540661a824611ada6bfe46f0dc
Isabella Paz <bellapaz.iorp@gmail.com>
2021-03-23 16:37:13 -0300

    atualizacao das rotas no App.js

--- src/App.js
+++ src/App.js
@@ -2,34 +2,42 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Switch, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Bebidas from './pages/Bebidas';
-import Comidas from './pages/Comidas';
-import NotFound from './pages/NotFound';
-import Perfil from './pages/Perfil';
-import Explorar from './pages/Explorar';
-import ReceitasFeitas from './pages/ReceitasFeitas';
+import {
+  Login,
+  Comidas,
+  Bebidas,
+  NotFound,
+  DetalhesReceita,
+  Explorar,
+  ExplorarIngredientes,
+  ExplorarBebidaOuComida,
+  ExplorarPorLocalDeOrigem,
+  Perfil,
+  ReceitasFeitas,
+  ReceitasFavoritas,
+  ReceitaEmProgresso,
+} from './pages';
 
 function App() {
   return (
     <div className="main">
       <Switch>
         <Route path="/comidas" component={ Comidas } />
-        <Route path="/comidas/:id/in-progress" component={ Bebidas } />
-        <Route path="/comidas/:id" component={ Comidas } />
+        <Route path="/comidas/:id/in-progress" component={ ReceitaEmProgresso } />
+        <Route path="/comidas/:id" component={ DetalhesReceita } />
         <Route path="/bebidas" component={ Bebidas } />
-        <Route path="/bebidas/:id/in-progress" component={ Bebidas } />
-        <Route path="/bebidas/:id" component={ Bebidas } />
-        <Route path="/explorar" component={ Explorar } />
+        <Route path="/bebidas/:id/in-progress" component={ ReceitaEmProgresso } />
+        <Route path="/bebidas/:id" component={ DetalhesReceita } />
+        <Route path="/explorar" component={ ExplorarBebidaOuComida } />
         <Route path="/explorar/comidas" component={ Explorar } />
         <Route path="/explorar/bebidas" component={ Explorar } />
-        <Route path="/explorar/comidas/ingredientes" component={ Explorar } />
-        <Route path="/explorar/bebidas/ingredientes" component={ Explorar } />
-        <Route path="/explorar/comidas/area" component={ Explorar } />
+        <Route path="/explorar/comidas/ingredientes" component={ ExplorarIngredientes } />
+        <Route path="/explorar/bebidas/ingredientes" component={ ExplorarIngredientes } />
+        <Route path="/explorar/comidas/area" component={ ExplorarPorLocalDeOrigem } />
         <Route path="/perfil" component={ Perfil } />
         <Route path="/receitas-feitas" component={ ReceitasFeitas } />
-        <Route path="/receitas-favoritas" component={ ReceitasFeitas } />
-        <Route path="/explorar/bebidas/area" component={ Explorar } />
+        <Route path="/receitas-favoritas" component={ ReceitasFavoritas } />
+        <Route path="/explorar/bebidas/area" component={ ExplorarPorLocalDeOrigem } />
         <Route exact path="/" component={ Login } />
         <Route path="*" component={ NotFound } />
       </Switch>

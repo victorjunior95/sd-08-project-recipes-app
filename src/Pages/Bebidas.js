08e8f3f7d4ebf194ee59fda169f2b00aad9edb8a
jnpefo <jnpefo@gmail.com>
2021-03-23 19:54:48 -0300

    Implementando as funcoes solicitadas

--- src/Pages/Bebidas.js
+++ src/Pages/Bebidas.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router';
+import { useSelector } from 'react-redux';
 import Header from './Header';
 import Footer from './Footer';
 
 function Bebidas() {
+  const estado = useSelector((state) => state.api);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (estado.data === 'SN') {
+      console.log('Sem filtros');
+    } else if (estado.data.drinks.length === 1) {
+      history.push(`/bebidas/${estado.data.drinks[0].idDrink}`);
+    } else if (estado.data.drinks.length > 1) {
+      console.log('fazer map');
+    }
+  }, [estado, history]);
+
   return (
     <>
       <Header title="Bebidas" />

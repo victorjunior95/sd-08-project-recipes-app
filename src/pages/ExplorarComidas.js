fe0085bccf5fedbbc5e546fc2115f93dc360a6af
vicoliveir <victorhugoqxd@gmail.com>
2021-03-23 17:41:53 -0300

    upando explore e explore drinks or meals 67-73

--- src/pages/ExplorarComidas.js
+++ src/pages/ExplorarComidas.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import HeaderLocation from '../components/Header';
 
@@ -7,9 +8,27 @@ class ExplorarComidas extends Component {
     return (
       <div>
         <HeaderLocation />
-        <br />
-        <br />
-        <br />
+        <div>
+        <Link
+          data-testid="explore-by-ingredient"
+          to="/explorar/comidas/ingredientes"
+        >
+          Por Ingredientes
+        </Link>
+        <Link
+          data-testid="explore-by-area"
+          to="/explorar/comidas/area"
+        >
+          Por Local de Origem
+        </Link>
+        <Link
+          data-testid="explore-surprise"
+          to="/explorar/comidas"
+        >
+          Me Surpreenda!
+        </Link>
+
+      </div>
         <Footer />
       </div>
     );

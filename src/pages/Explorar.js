a788858c7a2dc4902ba1cfbd20e967235b331bae
Rita Jeveaux <ritajeveaux@gmail.com>
2021-03-22 21:10:51 -0300

    Requisitos67-69

--- src/pages/Explorar.js
+++ src/pages/Explorar.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 
 function Explorar() {
+  const history = useHistory();
   return (
     <div>
       <h1>Explorar</h1>
+      <button
+        type="button"
+        data-testid="explore-food "
+        onClick={ () => {
+          history.push('/comidas');
+        } }
+      >
+        Explorar Comidas
+      </button>
+      <button
+        type="button"
+        data-testid="explore-drinks"
+        onClick={ () => {
+          history.push('/bebidas');
+        } }
+      >
+        Explorar Bebidas
+      </button>
       <Footer />
     </div>
   );

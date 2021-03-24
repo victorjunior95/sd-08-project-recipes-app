8a8d6da83e3ab6d0a3496e1b9bd87de8a80edb38
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-03-23 22:47:53 -0300

    Corrige endpoint e adiciona fetch na api de bebidas

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -22,8 +22,44 @@ export async function fetchFoodApiByIngredient(ingredient) {
   return meals;
 }
 
-export async function fetchRandomFood() {
-  const { meals } = await fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+export async function fetchFoodRecomendation() {
+  const { meals } = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=')
     .then((response) => response.json());
-  return meals;
+  const MIN = 0;
+  const MAX = 6;
+  const sixMeals = [...meals].slice(MIN, MAX);
+  return [...sixMeals];
+}
+
+export async function fetchDrinkApiByName(name) {
+  const { drinks } = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`)
+    .then((response) => response.json());
+  return drinks;
+}
+
+export async function fetchDrinkApiByLetter(letter) {
+  const { drinks } = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`)
+    .then((response) => response.json());
+  return drinks;
+}
+
+export async function fetchDrinkApiById(id) {
+  const { drinks } = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
+    .then((response) => response.json());
+  return drinks[0];
+}
+
+export async function fetchDrinkApiByIngredient(ingredient) {
+  const { drinks } = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${ingredient}`)
+    .then((response) => response.json());
+  return drinks;
+}
+
+export async function fetchDrinkRecomendation() {
+  const { drinks } = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=')
+    .then((response) => response.json());
+  const MIN = 0;
+  const MAX = 6;
+  const sixDrinks = [...drinks].slice(MIN, MAX);
+  return [...sixDrinks];
 }

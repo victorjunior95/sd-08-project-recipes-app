27ea4529fdb5a12c17fdf9b37e1389dbc8564bac
Marcela <marcela9908@gmail.com>
2021-03-24 17:23:15 -0300

    Estilizaçao Tela de Login

--- src/pages/Comidas.jsx
+++ src/pages/Comidas.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import styles from '../styles/comidas.module.css';
 
 function Comidas() {
-  return (<h1>BEMVINDO</h1>);
+  return (
+    <div>
+      <h1>BEMVINDO</h1>
+    </div>
+
+  );
 }
 
 export default Comidas;

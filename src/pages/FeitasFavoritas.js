78a310d5be32346f75529e8822d5ca5ad57ddf81
hugobrr <hugobrr@gmail.com>
2021-03-24 16:03:32 -0300

    ehhh ehh hii Garotão

---
+++ src/pages/FeitasFavoritas.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function FeitasFavoritas() {
+  return (
+    <div>
+      FeitasFavoritas
+    </div>
+  );
+}
---
+++ src/pages/Ingredientes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Ingredientes() {
+  return (
+    <div>
+      Ingredientes
+    </div>
+  );
+}

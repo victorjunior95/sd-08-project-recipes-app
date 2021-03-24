7dc68e8254e621d2b3ce9c87e2ff693989e857c2
hugobrr <hugobrr@gmail.com>
2021-03-24 19:32:03 -0300

    Requisito do 54 ao 59 passando com exessão do segundo sub requisito do 57

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import copy from 'clipboard-copy';
 import { useHistory } from 'react-router-dom';
+import shareIcon from '../images/shareIcon.svg';
 
-const Card = ({ id, index, name, thumbnail, isFood }) => {
+const Card = ({ id,
+  index, name, thumbnail, isFood, category, doneDate, tags, area, alcoholic }) => {
   const history = useHistory();
+  const path = history.location.pathname;
+  let isClicked = false;
   //   console.log(history);
   //   console.log(index);
   //   console.log(name);
@@ -16,33 +21,89 @@ const Card = ({ id, index, name, thumbnail, isFood }) => {
     }
     console.log(history);
   }
-  return (
-    <button
-      type="button"
-      onClick={ () => handleClick() }
-      data-testid={ `${index}-recipe-card` }
-    >
-      <p data-testid={ `${index}-card-name` }>{name}</p>
-      <img
+  let urlDetails = '';
+  if (isFood) {
+    urlDetails = `http://localhost:3000/comidas/${id}`;
+  } else {
+    urlDetails = `http://localhost:3000/bebidas/${id}`;
+  }
+  if (path === '/bebidas' || path === '/comidas') {
+    return (
+      <button
+        type="button"
+        onClick={ () => handleClick() }
+        data-testid={ `${index}-recipe-card` }
+      >
+        {/* {console.log(history.location.pathname)} */}
+        <p data-testid={ `${index}-card-name` }>{name}</p>
+        <img
         // style={ `max-width:200px;
         //     max-height:150px;
         //     width: auto;
         //     height: auto ;` }
+          width="320"
+          height="205"
+          src={ thumbnail }
+          alt="Foto da receita"
+          data-testid={ `${index}-card-img` }
+        />
+      </button>
+    );
+  } return (
+    <div data-testid={ `${index}-recipe-card` }>
+
+      <input
+        type="image"
+        onClick={ () => handleClick() }
         width="320"
         height="205"
         src={ thumbnail }
         alt="Foto da receita"
-        data-testid={ `${index}-card-img` }
+        data-testid={ `${index}-horizontal-image` }
       />
-    </button>
+      {isFood
+        ? <p data-testid={ `${index}-horizontal-top-text` }>{`${area} - ${category}`}</p>
+        : <p data-testid={ `${index}-horizontal-top-text` }>{alcoholic}</p>}
+      <a data-testid={ `${index}-horizontal-name` } href={ urlDetails }>{name}</a>
+      <p data-testid={ `${index}-horizontal-done-date` }>{doneDate}</p>
+      <input
+        type="image"
+        data-testid={ `${index}-horizontal-share-btn` }
+        src={ shareIcon }
+        alt="share"
+        onClick={ () => { copy(urlDetails); isClicked = true; } }
+      />
+      <p hidden={ isClicked }>Link copiado!</p>
+      { tags.map((tagName, tagInex) => (
+        <p
+          key={ tagInex }
+          data-testid={ `${index}-${tagName}-horizontal-tag` }
+        >
+          {tagName}
+        </p>
+      ))}
+    </div>
   );
 };
+Card.defaultProps = {
+  category: '',
+  doneDate: '',
+  tags: [],
+  area: '',
+  alcoholic: '',
+};
+
 Card.propTypes = {
   id: PropTypes.string.isRequired,
   index: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
   thumbnail: PropTypes.string.isRequired,
   isFood: PropTypes.bool.isRequired,
+  category: PropTypes.string,
+  doneDate: PropTypes.string,
+  tags: PropTypes.arrayOf(PropTypes.string),
+  area: PropTypes.string,
+  alcoholic: PropTypes.string,
 };
 
 export default Card;
--- src/pages/FeitasFavoritas.js
+++ src/pages/FeitasFavoritas.js
@@ -1,9 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import Card from '../components/Card';
 
 export default function FeitasFavoritas() {
-  return (
-    <div>
-      FeitasFavoritas
-    </div>
-  );
+  const history = useHistory();
+  const path = history.location.pathname;
+  let listCart = [];
+  listCart = JSON.parse(localStorage.getItem('doneRecipes'));
+  const [filteredList, setFilteredList] = useState(listCart);
+  useEffect(() => {
+    setFilteredList(listCart);
+  }, []);
+  if (path === '/receitas-feitas') {
+    return (
+      <div>
+        <label htmlFor="All">
+          <input
+            type="radio"
+            name="filter"
+            value="All"
+            id="All"
+            data-testid="filter-by-all-btn"
+            onClick={ () => setFilteredList(listCart) }
+          />
+          All
+        </label>
+        <label htmlFor="Food">
+          <input
+            type="radio"
+            name="filter"
+            value="Food"
+            id="Food"
+            data-testid="filter-by-food-btn"
+            onClick={ () => setFilteredList(listCart.filter((a) => a.type === 'comida')) }
+          />
+          Food
+        </label>
+        <label htmlFor="Drinks">
+          <input
+            type="radio"
+            name="filter"
+            value="Drinks"
+            id="Drinks"
+            data-testid="filter-by-drink-btn"
+            onClick={ () => setFilteredList(listCart.filter((a) => a.type === 'bebida')) }
+          />
+          Drinks
+        </label>
+        { filteredList.map((card, index) => (
+          <Card
+            key={ card.id }
+            id={ card.id }
+            index={ index }
+            name={ card.name }
+            thumbnail={ card.image }
+            isFood={ card.type === 'comida' }
+            category={ card.category }
+            doneDate={ card.doneDate }
+            tags={ card.tags }
+            area={ card.area }
+            alcoholic={ card.alcoholicOrNot }
+          />
+        ))}
+
+      </div>
+    );
+  }
 }

bca652a58f35e5774a9ae12407b73e38ea01d3d9
rodolfo_oliveira <rod.vgo@gmail.com>
2021-03-24 16:44:08 -0300

    destructuring

--- src/pages/foods/RecipesFood.js
+++ src/pages/foods/RecipesFood.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Header from '../../components/Header';
+import RecipesContext from '../../ContextApi/RecipesContext';
 
 function RecipesFood() {
+  const { recipes } = useContext(RecipesContext);
+  console.log(recipes);
+
   return (
     <div>
       <Header title="Comidas" />
+      <ul>
+        {/* {recipes.length > 0 ? Object.values(recipes).map((recipe, i) => <li key={ i }><img src={ recipe.meals.strMealThumb } alt="img" /></li>) : null} */}
+      </ul>
     </div>
   );
 }

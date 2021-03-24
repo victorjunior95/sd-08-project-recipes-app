3ebed94b8387aeeb2f686faf6d2061f87f286cdf
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2021-03-24 16:53:53 -0300

    Completa telas de explorar

--- src/services/RequisicaoApi.js
+++ src/services/RequisicaoApi.js
@@ -1,5 +1,27 @@
-export default async function fetchRecipes(endpoint, searchType, radioValue, inputText) {
+export async function fetchRecipes(endpoint, searchType, radioValue, inputText) {
   const url = `https://www.${endpoint}.com/api/json/v1/1/${searchType}.php?${radioValue}=${inputText}`;
+  try {
+    const requestReturn = await fetch(url);
+    const requestObject = await requestReturn.json();
+    return requestObject;
+  } catch (error) {
+    return { recipe: null };
+  }
+}
+
+export async function fetchRandomRecipe(endpoint) {
+  const url = `https://www.${endpoint}.com/api/json/v1/1/random.php`;
+  try {
+    const requestReturn = await fetch(url);
+    const requestObject = await requestReturn.json();
+    return requestObject;
+  } catch (error) {
+    return { recipe: null };
+  }
+}
+
+export async function fetchListByFilter(endpoint, filter) {
+  const url = `https://www.${endpoint}.com/api/json/v1/1/list.php?${filter}=list`;
   try {
     const requestReturn = await fetch(url);
     const requestObject = await requestReturn.json();

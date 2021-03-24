4d175cbd9271e5623cdcd1b027f080b8fcbe2d61
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-03-24 19:26:24 -0300

    recomendaçoes aparecendo

---
+++ src/components/salva.js
@@ -0,0 +1,10 @@
+const [startedRecipes, setStartedRecipes] = useState([]);
+
+  const addStartedRecipes = (newId) => {
+    setStartedRecipes([...startedRecipes, newId]);
+  };
+
+  const removeStartedRecipes = (newId) => {
+    const newArray = startedRecipes.filter((elem) => elem !== newId);
+    setStartedRecipes(newArray);
+  };

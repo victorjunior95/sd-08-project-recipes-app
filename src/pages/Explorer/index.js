4f0d4c86d93742e22b3d4511f39440d01e3d2127
Lucas-PC <lucasmvnascimento@gmail.com>
2021-03-24 18:14:02 -0300

    Req 67 - 69 - Implementing Explorer Page - Buttons

--- src/pages/Explorer/index.js
+++ src/pages/Explorer/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 
@@ -6,6 +7,12 @@ export default function Explorer() {
   return (
     <div>
       <Header title="Explorar" />
+      <Link to="/explorar/comidas">
+        <button type="button" data-testid="explore-food">Explorar Comidas</button>
+      </Link>
+      <Link to="/explorar/bebidas">
+        <button type="button" data-testid="explore-drinks">Explorar Bebidas</button>
+      </Link>
       <Footer />
     </div>
   );

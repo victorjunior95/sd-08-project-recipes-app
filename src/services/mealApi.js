96f52ecc94cc34680c00a705c61aea9ee1e1c339
Paulo Simões <paulohbsimoes@gmail.com>
2021-03-24 15:03:06 -0300

    Adiciona funcionalidades a api de comidas

--- src/services/mealApi.js
+++ src/services/mealApi.js
@@ -2,6 +2,18 @@ import fetchEndpoint from './utils';
 
 const BASE_URL = 'https://www.themealdb.com/api/json/v1/1';
 
+export const getIngredients = (ingredient) => (
+  fetchEndpoint(`${BASE_URL}/filter.php?i=${ingredient}`)
+);
+
+export const getCategories = (ingredient) => (
+  fetchEndpoint(`${BASE_URL}/filter.php?c=${ingredient}`)
+);
+
+export const getAreas = (ingredient) => (
+  fetchEndpoint(`${BASE_URL}/filter.php?a=${ingredient}`)
+);
+
 export const getByIngredient = (ingredient) => (
   fetchEndpoint(`${BASE_URL}/filter.php?i=${ingredient}`)
 );
@@ -16,4 +28,8 @@ export const getByFirstLetter = (firstLetter) => (
 
 export const getRandomMeal = () => (
   fetchEndpoint(`${BASE_URL}/random.php`)
+);
+
+export const getMealById = (id) => (
+  fetchEndpoint(`${BASE_URL}/lookup.php?i=${id}`)
 );

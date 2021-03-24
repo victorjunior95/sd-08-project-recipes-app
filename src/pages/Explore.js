393628ece49964b36ec338465f4b426b756d8420
Cleberton <clebertonfgc@gmail.com>
2021-03-24 16:25:49 -0300

    Finalizado com 'css da tela de explorar comidas e bebidas by: Paulo'

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -5,23 +5,29 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Container from '../components/Container';
 
+import styles from '../styles/pages/Explore.module.css';
+
 const Explore = () => (
-  <Container>
-    <Header title="Explorar" />
-      <Link 
-       to="/explorar/comidas"
-       data-testid="explore-food"
+  <div className={ styles.div }>
+    <Container>
+      <Header title="Explorar" />
+      <Link
+        className={ styles.buttons }
+        to="/explorar/comidas"
+        data-testid="explore-food"
       >
         Explorar Comidas
       </Link>
       <Link
-       to="/explorar/bebidas"
-       data-testid="explore-drinks"
+        className={ styles.buttons }
+        to="/explorar/bebidas"
+        data-testid="explore-drinks"
       >
         Explorar Bebidas
       </Link>
-    <Footer />
-  </Container>
+      <Footer />
+    </Container>
+  </div>
 );
 
 export default Explore;

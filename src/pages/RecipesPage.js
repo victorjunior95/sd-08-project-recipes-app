0e6cf8eb6a42dad52b902f5fbc80e1c66b70f4b6
albertassi88 <albertassi88@gmail.com>
2021-03-24 15:17:29 -0300

    Alteracoes

--- src/pages/RecipesPage.js
+++ src/pages/RecipesPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { Redirect, useHistory } from 'react-router';
+import { useHistory } from 'react-router';
 import useFetchDrinkRecipes from '../hooks/useFetchDrinkRecipes';
 import useFetchMealRecipes from '../hooks/useFetchMealRecipes';
 

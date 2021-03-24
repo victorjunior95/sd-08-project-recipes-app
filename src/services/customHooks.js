3c5e89b825637e9179698ac4572025263596e133
gabrieldeori <deori.pessoal@gmail.com>
2021-03-24 16:33:48 -0300

    :package: customHook Verifica se é comida ou bebida

---
+++ src/services/customHooks.js
@@ -0,0 +1,11 @@
+import useHistory from 'react-router-dom';
+
+export function useIsMeal() {
+  const history = useHistory();
+  const { location: { pathname } } = history;
+  return pathname.includes('comida');
+}
+
+export function dummy() {
+  console.log('dummy');
+}

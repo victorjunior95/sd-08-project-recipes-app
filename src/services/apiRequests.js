2e0d246b63c8b367ff0102cc174d78975993f5f0
liisaambrosen <liisa.ambrosen@gmail.com>
2021-03-24 11:33:50 -0300

    req 32 e inicio do 33

--- src/services/apiRequests.js
+++ src/services/apiRequests.js
@@ -9,3 +9,9 @@ export async function requestDrinksList() {
   const { drinks } = response;
   return drinks;
 }
+
+export async function requestMealRecipe(id) {
+  const response = await ((await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)).json());
+  const { meals } = response;
+  return meals;
+}

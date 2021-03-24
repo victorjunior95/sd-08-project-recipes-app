d34b888970c8f0e44a68b9e354d41e10ecae14e3
Arnaelcio <arnaelcio.gomes@gmail.com>
2021-03-24 20:14:59 -0300

    for update

--- src/pages/DetailsDrink.js
+++ src/pages/DetailsDrink.js
@@ -1,36 +1,119 @@
 import React, { useEffect, useContext, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { DataDrinksContext } from '../context/ContextDrinks';
-import { getDrinkRecipesDetails } from '../services/getAPIs';
-
+import { getDrinksDetails, getMealByName } from '../services/getAPIs';
+import { LoginAndFoodContext } from '../context/ContextFood';
+import shareIcon from '../images/shareIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 import './DetailsDrink.css';
 
 function DetailsDrink() {
-  const dataContext = useContext(DataDrinksContext);
-  const { drinks } = dataContext;
+  const dataFood = useContext(LoginAndFoodContext);
+  const { meals } = dataFood;
   const Params = useParams();
   const [drinkDetail, setDrinkDetail] = useState([]);
   useEffect(() => {
-    const result = drinks.filter((drink) => drink.idDrink === Params.id);
-    const getIdDrink = result.map((item) => item.idDrink);
-    async function fetchDetailsRecipe() {
-      const getDetailsRecipe = await getDrinkRecipesDetails(getIdDrink);
-      setDrinkDetail(getDetailsRecipe);
+    async function fetchDetails() {
+      const saveDetail = await getDrinksDetails(Params.id);
+      console.log(saveDetail);
+      setDrinkDetail(saveDetail);
     }
-    fetchDetailsRecipe();
-  }, [drinks, Params.id]);
+    getMealByName('');
+    fetchDetails();
+  }, []);
 
-  // console.log(drinkDetail);
+  const sizeOfLength = 3;
+  const startOfSlice = 0;
+  const endOfSlice = 2;
+  const measure = Object.entries(drinkDetail).reduce(
+    (acc, [key, value], index) => {
+      if (key.includes('strMeasure') && value) {
+        return acc.concat(value);
+      }
+      return acc;
+    },
+    [],
+  );
+
   return (
     <div>
       <div className="container-card-drink-details">
-        {drinkDetail.map((drink) => (
-          <div className="card-drink-details" key={ drink.idDrink }>
-            <img src={ drink.strDrinkThumb } alt="thumbnails-drink" />
-            <h2>{drink.strDrink}</h2>
-          </div>
-        ))}
+        <div className="card-drink-details" key={ drinkDetail.idDrink }>
+          <img
+            data-testid="recipe-photo"
+            src={ drinkDetail.strDrinkThumb }
+            alt="thumbnails-drink"
+          />
+          <h2 data-testid="recipe-title">{drinkDetail.strDrink}</h2>
+          <p data-testid="recipe-category">
+            {drinkDetail.strCategory}
+            {' '}
+            -
+            {drinkDetail.strAlcoholic}
+          </p>
+          <h3>Ingredients</h3>
+          <ul>
+            {Object.entries(drinkDetail).reduce((acc, [key, value], index) => {
+              if (key.includes('strIngredient') && value) {
+                return acc.concat(
+                  <li
+                    data-testid={ `${acc.length}-ingredient-name-and-measure` }
+                    key={ index }
+                  >
+                    {measure[acc.length]}
+                    {value}
+                    {' '}
+                    -
+                  </li>,
+                );
+              }
+              return acc;
+            }, [])}
+          </ul>
+          <h4>Instructions</h4>
+          <p data-testid="instructions">{drinkDetail.strInstructions}</p>
+          <h4>Video</h4>
+          <video data-testid="video" src={ drinkDetail.strYoutube }>
+            <track
+              default
+              kind="captions"
+              srcLang="pt-br"
+              src={ drinkDetail.strYoutube }
+            />
+          </video>
+          <h4>Recomendadas</h4>
+          {meals.length > sizeOfLength
+            && meals
+              .slice(startOfSlice, endOfSlice)
+              .map((meal, index) => (
+                <img
+                  key={ meal.idMeal }
+                  data-testid={ `${index}-recomendation-card` }
+                  src={ meal.strMealsThumb }
+                  alt="recomendations"
+                />
+              ))}
+          {/*
+
+          <img
+            // data-testid={ `${index}-recomendation-card` }
+            src=""
+            alt="recomendations"
+          /> */}
+          <button data-testid="start-recipe-btn" type="button">
+            Iniciar Receita
+          </button>
+        </div>
       </div>
+      <button type="button">
+        <img data-testid="share-btn" src={ shareIcon } alt="share-icon" />
+      </button>
+      <button type="button">
+        <img
+          data-testid="favorite-btn"
+          src={ whiteHeartIcon }
+          alt="favorite-icon"
+        />
+      </button>
     </div>
   );
 }
--- src/pages/DetailsMeal.js
+++ src/pages/DetailsMeal.js
@@ -1,43 +1,108 @@
 import React, { useEffect, useContext, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { LoginAndFoodContext } from '../context/ContextFood';
-import { getMealRecipesDetails } from '../services/getAPIs';
+import { DataDrinksContext } from '../context/ContextDrinks';
+import { getMealsDetails, getDrinkByName } from '../services/getAPIs';
+import shareIcon from '../images/shareIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+// import blackHeartIcon from '../images/blackHeartIcon.svg';
 import './DetailsMeal.css';
 
 function DetailsMeal() {
-  const dataContext = useContext(LoginAndFoodContext);
-  const { meals } = dataContext;
+  const dataDrinks = useContext(DataDrinksContext);
+  const { drinks } = dataDrinks;
   const Params = useParams();
-  const [mealDetail, setMealDetail] = useState([{}]);
+  const [mealDetail, setMealDetail] = useState([]);
   useEffect(() => {
-    const result = meals.filter((meal) => meal.idMeal === Params.id);
-    const getIdMeal = result.map((item) => item.idMeal);
-    async function fetchDetailsRecipe() {
-      const getDetailsRecipe = await getMealRecipesDetails(getIdMeal);
-      setMealDetail(getDetailsRecipe);
+    async function fetchDetails() {
+      const saveDetail = await getMealsDetails(Params.id);
+      setMealDetail(saveDetail);
     }
-    fetchDetailsRecipe();
-  }, [meals, Params.id]);
+    fetchDetails();
+  }, []);
 
-  // const teste = Object.keys(mealDetail[0];
+  const sizeOfLength = 3;
+  const startOfSlice = 0;
+  const endOfSlice = 2;
 
-  // console.log(teste.filter(item => item.includes('strIngredient')))
-  console.log(
-    Object.keys(mealDetail).forEach((element) => {
-      element.includes('strIngredient');
-    }),
+  const measure = Object.entries(mealDetail).reduce(
+    (acc, [key, value], index) => {
+      if (key.includes('strMeasure') && value) {
+        return acc.concat(value);
+      }
+      return acc;
+    },
+    [],
   );
 
   return (
     <div>
       <div className="container-card-meal-details">
-        {/* {mealDetail.map((meal) => ( */}
-        {/* <div className="card-meal-details" key={meal.idMeal}> */}
-        {/* <img src={meal.strMealThumb} alt="thumbnails-meal" /> */}
-        {/* <h2>{meal.strMeal}</h2> */}
-        {/* </div> */}
-        {/* ))} */}
+        <div className="card-meal-details" key={ mealDetail.idMeal }>
+          <img
+            data-testid="recipe-photo"
+            src={ mealDetail.strMealThumb }
+            alt="thumbnails-meal"
+          />
+          <h2 data-testid="recipe-title">{mealDetail.strMeal}</h2>
+          <p data-testid="recipe-category">{mealDetail.strCategory}</p>
+          <h3>Ingredients</h3>
+          <ul>
+            {Object.entries(mealDetail).reduce((acc, [key, value], index) => {
+              if (key.includes('strIngredient') && value) {
+                return acc.concat(
+                  <li
+                    data-testid={ `${acc.length}-ingredient-name-and-measure` }
+                    key={ index }
+                  >
+                    {value}
+                    {' '}
+                    -
+                    {measure[acc.length]}
+                  </li>,
+                );
+              }
+              return acc;
+            }, [])}
+          </ul>
+
+          <h4>Instructions</h4>
+          <p data-testid="instructions">{mealDetail.strInstructions}</p>
+          <h4>Video</h4>
+          <video data-testid="video" src={ mealDetail.strYoutube }>
+            <track
+              default
+              kind="captions"
+              srcLang="pt-br"
+              src={ mealDetail.strYoutube }
+            />
+          </video>
+          <h4>Recomendadas</h4>
+          {drinks.length > sizeOfLength
+            && drinks
+              .slice(startOfSlice, endOfSlice)
+              .map((drink, index) => (
+                <img
+                  key={ drink.idDrink }
+                  data-testid={ `${index}-recomendation-card` }
+                  src={ drink.strDrinkThumb }
+                  alt="recomendations"
+                />
+              ))}
+          <button data-testid="start-recipe-btn" type="button">
+            Iniciar Receita
+          </button>
+        </div>
       </div>
+      <button type="button">
+        <img data-testid="share-btn" src={ shareIcon } alt="share-icon" />
+      </button>
+      <button type="button">
+        <img
+          data-testid="favorite-btn"
+          src={ whiteHeartIcon }
+          alt="favorite-icon"
+        />
+      </button>
     </div>
   );
 }

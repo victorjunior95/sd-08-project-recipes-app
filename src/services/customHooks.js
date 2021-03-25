cc294277fc0f8bfc190badfe015a63240a467534
gabrieldeori <deori.pessoal@gmail.com>
2021-03-25 16:16:52 -0300

    RE-RUNNNNNN

--- src/services/customHooks.js
+++ src/services/customHooks.js
@@ -5,10 +5,6 @@ export function useIsMeal() {
   return pathname.includes('comida');
 }
 
-export function dummy() {
-  console.log('dummy');
-}
-
 export function dummy2() {
   console.log('dummy2');
 }

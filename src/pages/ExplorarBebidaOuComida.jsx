2d2f9d64598fec78487e80fb3a7b5b98754d5222
Isabella Paz <bellapaz.iorp@gmail.com>
2021-03-25 18:17:32 -0300

    tela explorar

--- src/pages/ExplorarBebidaOuComida.jsx
+++ src/pages/ExplorarBebidaOuComida.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import ContextReceitas from '../context/ContextReceitas';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
@@ -9,12 +10,30 @@ function ExplorarBebidasOuComidas() {
     mudarStatusBotaoPesquisa,
   } = useContext(ContextReceitas);
   useEffect(() => {
-    enviarTituloDaPagina('Explorar Bebidas Ou Comidas');
+    enviarTituloDaPagina('Explorar');
     mudarStatusBotaoPesquisa(false);
   }, []);
   return (
     <div>
       <Header />
+      <div>
+        <Link to="/explorar/comidas">
+          <button
+            type="button"
+            data-testid="explore-food"
+          >
+            Explorar Comidas
+          </button>
+        </Link>
+        <Link to="/explorar/bebidas">
+          <button
+            type="button"
+            data-testid="explore-drinks"
+          >
+            Explorar Bebidas
+          </button>
+        </Link>
+      </div>
       <Footer />
     </div>
   );

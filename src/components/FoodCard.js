21da83c14516285a8f3b4480f5d5afc2500dac54
Rafael <rafaeljavaro750@gmail.com>
2021-03-25 14:12:14 -0300

    Transforma FoodCard em link para comidas/idMeal

--- src/components/FoodCard.js
+++ src/components/FoodCard.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 export default function FoodCard({ recipes }) {
   const { strMeal, idMeal, strMealThumb } = recipes;
+
   return (
-    <div key={ strMeal } data-testid={ `${idMeal}-recipe-card` }>
-      <img
-        src={ strMealThumb }
-        alt={ strMeal }
-        data-testid={ `${idMeal}-card-img` }
-      />
-      <p data-testid={ `${idMeal}-card-name` }>{strMeal}</p>
-    </div>
+    <Link to={ `comidas/${idMeal} ` }>
+      <div key={ strMeal } data-testid={ `${idMeal}-recipe-card` }>
+        <img
+          src={ strMealThumb }
+          alt={ strMeal }
+          data-testid={ `${idMeal}-card-img` }
+        />
+        <p data-testid={ `${idMeal}-card-name` }>{strMeal}</p>
+      </div>
+    </Link>
   );
 }
 

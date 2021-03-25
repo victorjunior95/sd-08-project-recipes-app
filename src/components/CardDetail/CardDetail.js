139615a138ef14e6366f786a3f756caefcbe610f
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-03-25 20:45:20 -0300

    Finish him

--- src/components/CardDetail/CardDetail.js
+++ src/components/CardDetail/CardDetail.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useContext } from 'react';
 import PropTypes from 'prop-types';
-
+import { useParams } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import ListGroup from 'react-bootstrap/ListGroup';
@@ -10,12 +10,10 @@ import blackHeartIcon from '../../images/blackHeartIcon.svg';
 import Context from '../../contextApi/Context';
 
 const CardDetails = ({ title, object, isLoading, location: { pathname } }) => {
-  const { receitaAtu,
-    setReceita } = useContext(Context);
-  const [isFavorite, setIsFavorite] = useState();
   const [localUrl, setLocalUrl] = useState('');
   const [favorite, setFavorite] = useState(false);
   const [save, setSave] = useState(false);
+  const { id } = useParams();
 
   useEffect(() => {
     const saveURL = () => {
@@ -30,72 +28,76 @@ const CardDetails = ({ title, object, isLoading, location: { pathname } }) => {
   };
 
   useEffect(() => {
-    const setFavoriteButton = () => {
-      const bla = JSON.parse(localStorage.getItem('favoriteRecipes'));
-      if (bla) {
-        const bla1 = JSON.parse(localStorage.getItem('favoriteRecipes'));
-        const { id } = bla1[0];
-        const url = `http://localhost:3000/${title.toLowerCase()}/${id}`;
-        setIsFavorite(url);
-      }
-    };
-    setFavoriteButton();
+    if (!localStorage.hasOwnProperty('favoriteRecipes')) {
+      localStorage.setItem('favoriteRecipes', JSON.stringify([]));
+    }
+    const local = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    if (local.length > 0 && local[0].id === id) {
+      setFavorite(true);
+    }
   }, []);
 
   const savetoFavoritest = () => {
     favorite === false ? setFavorite(true) : setFavorite(false);
-
-    // const arrayInicial = JSON.parse(localStorage.getItem('favoriteRecipes'));
-    const { idMeal, strCategory, strArea, strMeal, strAlcoholic, strDrinkThumb, strDrink, idDrink, strMealThumb } = object;
+    const {
+      idMeal,
+      strCategory,
+      strArea,
+      strMeal,
+      strAlcoholic,
+      strDrinkThumb,
+      strDrink,
+      idDrink,
+      strMealThumb,
+    } = object;
 
     if (title === 'Comidas' && favorite === false) {
-      const Objectid = { id: idMeal, type: 'comida', area: strArea, category: strCategory, alcoholicOrNot: '', name: strMeal, image: strMealThumb };
+      const Objectid = {
+        id: idMeal,
+        type: 'comida',
+        area: strArea,
+        category: strCategory,
+        alcoholicOrNot: '',
+        name: strMeal,
+        image: strMealThumb,
+      };
       localStorage.setItem('favoriteRecipes', JSON.stringify([Objectid]));
-    } if (title === 'Bebidas' && favorite === false) {
-      const Objectid = { id: idDrink, type: 'bebida', area: '', category: strCategory, alcoholicOrNot: strAlcoholic, name: strDrink, image: strDrinkThumb };
+    }
+    if (title === 'Bebidas' && favorite === false) {
+      const Objectid = {
+        id: idDrink,
+        type: 'bebida',
+        area: '',
+        category: strCategory,
+        alcoholicOrNot: strAlcoholic,
+        name: strDrink,
+        image: strDrinkThumb,
+      };
       localStorage.setItem('favoriteRecipes', JSON.stringify([Objectid]));
     }
     const objetoAtual = JSON.parse(localStorage.getItem('favoriteRecipes'));
-    const filter = objetoAtual.filter((elemento) => elemento.id !== objetoAtual[0].id);
-    favorite === true && localStorage.setItem('favoriteRecipes', JSON.stringify(filter));
-  };
-  const renderOrNot = () => {
-    if (favorite === false || localUrl === isFavorite) {
-      console.log('branco');
-      console.log(isFavorite);
-      return (
-        <img
-          src={ whiteHeartIcon }
-          alt="Profile icon"
-        />
-      );
-    } if (favorite || localUrl === isFavorite) {
-      console.log('preto');
-      console.log(isFavorite);
-      return (
-        <img
-          src={ blackHeartIcon }
-          alt="Profile icon"
-        />
-
-      );
-    }
+    const filter = objetoAtual.filter(
+      (elemento) => elemento.id !== objetoAtual[0].id,
+    );
+    favorite === true &&
+      localStorage.setItem('favoriteRecipes', JSON.stringify(filter));
   };
   const renderIngredientList = () => {
     const listKeys = Object.keys(object);
     const ingredients = listKeys.filter((key) => key.includes('strIngredient'));
 
     const listKeysMeasure = Object.keys(object);
-    const measures = listKeysMeasure.filter((key) => key.includes('strMeasure'));
+    const measures = listKeysMeasure.filter((key) =>
+      key.includes('strMeasure'),
+    );
 
     return ingredients.map((ingredient, index) => {
       if (object[ingredient]) {
         return (
-          <ListGroup.Item
-            data-testid={ `${index}-ingredient-name-and-measure` }
-          >
+          <ListGroup.Item data-testid={`${index}-ingredient-name-and-measure`}>
             {`${object[ingredient]} - ${object[measures[index]]}`}
-          </ListGroup.Item>);
+          </ListGroup.Item>
+        );
       }
       return true;
     });
@@ -108,7 +110,7 @@ const CardDetails = ({ title, object, isLoading, location: { pathname } }) => {
           data-testid="video"
           width="300"
           height="200"
-          src={ object.strYoutube }
+          src={object.strYoutube}
           frameBorder="0"
           allowFullScreen
           title="vídeo"
@@ -118,68 +120,58 @@ const CardDetails = ({ title, object, isLoading, location: { pathname } }) => {
   };
 
   if (isLoading === true) {
+    return <div>Loading</div>;
+  }
+  if (isLoading === false && object !== {}) {
     return (
-      <div>Loading</div>
-    );
-  } if (isLoading === false && object !== {}) {
-    return (
-      <Card
-        style={ { width: '18rem' } }
-      >
+      <Card style={{ width: '18rem' }}>
         <Card.Img
           data-testid="recipe-photo"
           variant="top"
-          src={ (title === 'Comidas') ? object.strMealThumb : object.strDrinkThumb }
+          src={title === 'Comidas' ? object.strMealThumb : object.strDrinkThumb}
         />
         <Card.Body>
-          <Card.Title
-            data-testid="recipe-title"
-          >
-            { (title === 'Comidas') ? object.strMeal : object.strDrink}
+          <Card.Title data-testid="recipe-title">
+            {title === 'Comidas' ? object.strMeal : object.strDrink}
           </Card.Title>
-
           <Button
             variant="outline-secondary"
             data-testid="share-btn"
-            onClick={ () => savetoClipboard() }
+            onClick={() => savetoClipboard()}
           >
-            {save ? 'Link copiado!'
-
-              : <img
-                  src={ shareIcon }
-                  alt="Profile icon"
-
-              />}
-
+            {save ? (
+              'Link copiado!'
+            ) : (
+              <img src={shareIcon} alt="Profile icon" />
+            )}
           </Button>
-
-          {' '}
           <Button
             variant="outline-secondary"
             data-testid="favorite-btn"
-            onClick={ () => savetoFavoritest() }
+            src={favorite ? blackHeartIcon : whiteHeartIcon}
+            onClick={() => savetoFavoritest()}
           >
-            { renderOrNot()}
+            {favorite === true ? (
+              <img src={blackHeartIcon} alt="Profile icon" />
+            ) : (
+              <img src={whiteHeartIcon} alt="Profile icon" />
+            )}
           </Button>
-          {' '}
           <Card.Text data-testid="recipe-category">
-            { (title === 'Comidas') ? object.strCategory : object.strAlcoholic}
+            {title === 'Comidas' ? object.strCategory : object.strAlcoholic}
           </Card.Text>
           <ListGroup>
             Ingredientes:
             {renderIngredientList()}
           </ListGroup>
           <Card.Text data-testid="instructions">
-            { object.strInstructions }
+            {object.strInstructions}
           </Card.Text>
           {renderVideo()}
           <Card.Text data-testid="0-recomendation-card">
             Aqui virá o card das recomendações
           </Card.Text>
-          <Button
-            variant="primary"
-            data-testid="start-recipe-btn"
-          >
+          <Button variant="primary" data-testid="start-recipe-btn">
             Iniciar receita
           </Button>
         </Card.Body>

a0e2ea85e5e64e0b3450554c66b33d4458dae3b3
Blucilha <blucilha.nascimento@gmail.com>
2021-03-25 16:47:13 -0300

    Pagina de Perfil Pronta

--- src/pages/explorer/Explorer.js
+++ src/pages/explorer/Explorer.js
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/footer/Footer';
 
-
 export default function Explorer() {
   return (
     <div>

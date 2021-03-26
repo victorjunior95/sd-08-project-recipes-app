37a64fca8ca21ee8f7e8c051e1ffc2b7545c9a24
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-03-26 17:19:07 -0300

    corrige lint

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -14,7 +14,6 @@ function Provider({ children }) {
   const [recommendedDrink, setRecommendedDrink] = useState([]);
   const [copied, setCopied] = useState(false);
 
-
   // const [startedRecipes, setStartedRecipes] = useState([]);
 
   // const addStartedRecipes = (newId) => {

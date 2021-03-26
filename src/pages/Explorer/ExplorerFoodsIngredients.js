20d345f61664618273c29525225a21ae906b3b47
JRGCast <joao.ralph1@gmail.com>
2021-03-26 18:10:34 -0300

    adicionada logica experimental em explorerFoodIngredients, mudado Header de explorer para parar o warning correspondente a currentPage

--- src/pages/Explorer/ExplorerFoodsIngredients.js
+++ src/pages/Explorer/ExplorerFoodsIngredients.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
+import { showCompleteLists } from '../../services/api';
 
 function ExplorerFoodsIngredients() {
+  const [ingredientsList, setIngredientsList] = useState([]);
+
+  useEffect(() => {
+    async function requestNSet() {
+      const requestList = await showCompleteLists('ingredients', 'Foods');
+      setIngredientsList(requestList);
+    } requestNSet();
+  }, []);
   return (
     <div>
       <Header name="Explorar Ingredientes" icon="true" />
-      ...
+      {console.log(ingredientsList)}
       <Footer />
     </div>
   );

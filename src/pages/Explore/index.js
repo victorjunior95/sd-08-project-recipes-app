2135e478bb924dd47e52a542d3a3af7e8abdf554
Breenolf <blf@ic.ufal.br>
2021-03-26 16:29:19 -0300

    :checkered-flag: requisitos 67, 68 e 69

--- src/pages/Explore/index.js
+++ src/pages/Explore/index.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Feed from '../../components/ExploreComponents/Feed';
+// import NotFound from '../NotFound/NotFound';
+
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
 function Explore({ history }) {
+  const { location: { pathname } } = history;
+
   return (
     <>
       <Header history={ history } />
-      <h1>Explorar</h1>
+      {pathname === '/explorar' && <Feed />}
+      {/* {path === '/explorar/comidas' && <Meals path={ path } />} */}
+      {/* {path === '/explorar/comidas/area' && <Area path={ path } />} */}
+      {/* {path === '/explorar/bebidas/area' && <NotFound path={ path } />} */}
+      {/* {path === '/explorar/bebidas' && <Drinks path={ path } />} */}
+      {/* {path === '/explorar/comidas/ingredientes' && <Ingredients path={ path } />} */}
+      {/* {path === '/explorar/bebidas/ingredientes' && <Ingredients path={ path } />} */}
       <Footer />
     </>
   );
 }
 
 Explore.propTypes = {
-  history: PropTypes.shape({}).isRequired,
+  history: PropTypes.shape({
+    location: PropTypes.shape({
+      pathname: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
 };
 
 export default Explore;

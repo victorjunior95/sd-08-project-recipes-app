544415e6a81ca1e2dd9f0a2add704e28fadb5e5c
Rafael Fernandes <rafaelsantanafernandes@gmail.com>
2021-03-26 12:58:08 -0300

    specs expore ingrtedients done

---
+++ src/components/CardIngredientsFoods.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import getIngredients from '../helpers/ingredientsApi';
+
+export default function CardIngredientsFoods({ index, name, isMeal }) {
+  const [setControl] = useState(false);
+  const [setRecipes] = useState([]);
+  const [setFilteredRecipes] = useState([]);
+  const history = useHistory();
+  const src = `https://www.themealdb.com/images/ingredients/${name}-Small.png`;
+
+  const fetchMeals = async (type, value, controlVar) => {
+    const result = await getIngredients(type, value);
+    setRecipes(result);
+    if (controlVar) setFilteredRecipes(result);
+    if (!controlVar) {
+      return null;
+    }
+    const resultReturn = {
+      result,
+      redirect: false,
+    };
+    if (result.meals === null) {
+      return alert('Sinto muito, não encontramos nenhuma receita para esses filtros.');
+    }
+    if (result.meals.length === 1) {
+      resultReturn.redirect = true;
+      setControl(true);
+      return resultReturn;
+    }
+    setControl(true);
+    return resultReturn;
+  };
+
+  const searchRecipes = async () => {
+    if (isMeal) {
+      await fetchMeals('ingredient', name, true);
+    }
+    history.push('/comidas');
+  };
+
+  return (
+    <div
+      className="recipe-card"
+      data-testid={ `${index}-ingredient-card` }
+    >
+      <button
+        type="button"
+        onClick={ searchRecipes }
+      >
+        <img
+          alt="Recipe Thumbnail"
+          data-testid={ `${index}-card-img` }
+          src={ src }
+          className="recipe-thumb"
+          height="250"
+        />
+        <h2
+          className="recipe-name"
+          data-testid={ `${index}-card-name` }
+        >
+          {name}
+        </h2>
+      </button>
+    </div>
+  );
+}
+
+CardIngredientsFoods.propTypes = {
+  index: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  isMeal: PropTypes.bool.isRequired,
+};
--- src/pages/ExplorarBebidasIngredientes.js
+++ src/pages/ExplorarBebidasIngredientes.js
@@ -1,19 +1,39 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import Header from '../components/Header';
 import Footer from '../components/Footer';
-import HeaderLocation from '../components/Header';
+import getDrinks from '../helpers/drinkApi';
+import CardIngredientsDrinks from '../components/CardIngredientsDrinks';
 
-class ExplorarBebidasIngredientes extends Component {
-  render() {
-    return (
-      <div>
-        <HeaderLocation />
-        <br />
-        <br />
-        <br />
-        <Footer />
-      </div>
-    );
-  }
+export default function DrinkIngredients() {
+  const [ingredients, setIngrediets] = useState('');
+  const zero = 0;
+  const twelve = 12;
+
+  useEffect(() => {
+    async function fetchIngredients() {
+      const response = await getDrinks('listIngredient', '');
+      setIngrediets(response.drinks);
+    }
+    fetchIngredients();
+  }, [setIngrediets]);
+
+  return (
+    <div>
+      <Header title="Explorar Ingredientes" />
+      { (ingredients.length > zero)
+        && ingredients.map((ingredient, index) => {
+          if (index < twelve) {
+            return (
+              <CardIngredientsDrinks
+                index={ index }
+                name={ ingredient.strIngredient1 }
+                key={ index }
+              />
+            );
+          }
+          return null;
+        })}
+      <Footer />
+    </div>
+  );
 }
-
-export default ExplorarBebidasIngredientes;

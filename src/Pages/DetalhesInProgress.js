c23ba3559fdf099d1391f0fcc4615fb52be5b2dc
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-03-26 18:38:09 -0300

    salva ingredients checked no local storage

--- src/Pages/DetalhesInProgress.js
+++ src/Pages/DetalhesInProgress.js
@@ -8,6 +8,7 @@ import Loading from '../components/Loading';
 import RecipeContext from '../context/RecipeContext';
 import LinkCopiado from '../components/LinkCopiado';
 import ButtonFinish from '../components/ButtonFinish';
+import IngredientsCheckbox from '../components/IngredientsCheckbox';
 
 function DetalhesInProgress() {
   const TWO_SECONDS = 2000;
@@ -34,30 +35,6 @@ function DetalhesInProgress() {
     setTimeout(() => {
       setLoading(false);
     }, TWO_SECONDS);
-  };
-
-  const getIngredients = () => {
-    const ingredientes = Object.entries(objDetail[0]);
-
-    const measure = ingredientes.filter((elem) => (
-      elem[0].includes('strMeasure') && elem[1] !== null && elem[1] !== ''
-    ));
-    const filtering = ingredientes.filter((element) => (
-      element[0].includes('strIngredient') && element[1] !== null && element[1] !== ''));
-
-    const results = filtering.map((elem, index) => (
-      <li
-        key={ elem[1] }
-        data-testid={ `${index}-ingredient-step` }
-      >
-        <label htmlFor={ elem[1] }>
-          <input type="checkbox" id={ elem[1] } />
-          {elem[1]}
-          <span>{measure[index] === undefined ? '' : measure[index][1]}</span>
-        </label>
-      </li>));
-
-    return results;
   };
 
   const handleCopied = () => {
@@ -107,9 +84,7 @@ function DetalhesInProgress() {
           alt={ objDetail[0].strDrink }
         />
       </div>
-      <ol className="ingredient-list">
-        { getIngredients() }
-      </ol>
+      <IngredientsCheckbox objDetail={ objDetail } id={ id } url={ urlText } />
       <p data-testid="instructions">{objDetail[0].strInstructions}</p>
       <ButtonFinish />
     </div>
@@ -147,9 +122,7 @@ function DetalhesInProgress() {
           alt={ objDetail[0].strMeal }
         />
       </div>
-      <ol className="ingredient-list">
-        { getIngredients() }
-      </ol>
+      <IngredientsCheckbox objDetail={ objDetail } id={ id } url={ urlText } />
       <p data-testid="instructions">{objDetail[0].strInstructions}</p>
       <ButtonFinish />
     </div>
---
+++ src/components/IngredientsCheckbox.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function IngredientsCheckbox({ objDetail, id, url }) {
+  const handleChange = ({ target }) => {
+    const ol = target.parentNode.parentNode.parentNode;
+    const lista = ol.childNodes;
+    const listaDeIngredientes = [];
+
+    lista.forEach((elem) => {
+      const checkbox = elem.firstChild.firstChild;
+      if (checkbox.checked) {
+        listaDeIngredientes.push(checkbox.id);
+      }
+    });
+
+    console.log(listaDeIngredientes);
+    const inProgressRecipes = JSON.parse(localStorage.getItem('inProgressRecipes'));
+    if (url.includes('comidas')) {
+      localStorage.setItem('inProgressRecipes', JSON.stringify({
+        ...inProgressRecipes,
+        meals: {
+          ...inProgressRecipes.meals,
+          [id]: listaDeIngredientes,
+        },
+      }));
+    }
+    if (url.includes('bebidas')) {
+      localStorage.setItem('inProgressRecipes', JSON.stringify({
+        ...inProgressRecipes,
+        cocktails: {
+          ...inProgressRecipes.cocktails,
+          [id]: listaDeIngredientes,
+        },
+      }));
+    }
+  };
+  const getIngredients = () => {
+    const ingredientes = Object.entries(objDetail[0]);
+
+    const measure = ingredientes.filter((elem) => (
+      elem[0].includes('strMeasure') && elem[1] !== null && elem[1] !== ''
+    ));
+    const filtering = ingredientes.filter((element) => (
+      element[0].includes('strIngredient') && element[1] !== null && element[1] !== ''));
+
+    const results = filtering.map((elem, index) => (
+      <li
+        name="li-ingredients"
+        key={ elem[1] }
+        data-testid={ `${index}-ingredient-step` }
+      >
+        <label htmlFor={ elem[1] }>
+          <input
+            onChange={ handleChange }
+            type="checkbox"
+            id={ elem[1] }
+          />
+          {elem[1]}
+          <span>{measure[index] === undefined ? '' : measure[index][1]}</span>
+        </label>
+      </li>));
+
+    return results;
+  };
+
+  return (
+    <ol className="ingredient-list">
+      {getIngredients()}
+    </ol>
+  );
+}
+
+IngredientsCheckbox.propTypes = {
+  id: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  objDetail: PropTypes.shape(
+    PropTypes.any,
+  ).isRequired,
+};
+
+export default IngredientsCheckbox;

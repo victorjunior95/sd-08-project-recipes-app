83625920866c051fda0d7b9b7a0aaff1c852d8dc
Bruno Martins <bruno.martins217@gmail.com>
2021-03-26 16:00:15 -0300

    Requisitos 83 e 87

--- src/containers/ExploreOrigination.jsx
+++ src/containers/ExploreOrigination.jsx
@@ -25,7 +25,6 @@ function ExploreOrigination() {
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [input]);
-  console.log(mealArea);
   return (
     <div>
       <components.Header title="Explorar Origem" />
--- src/containers/Profile.jsx
+++ src/containers/Profile.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import components from '../components/index';
 
 function Profile() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    localStorage.setItem('email', 'email@mail.com');
+    setData(localStorage.getItem('email'));
+  }, []);
   return (
     <div>
       <components.Header title="Perfil" />
       <main className="explore-container">
-        <h3 data-testid="profile-email">Email</h3>
+        <h3 data-testid="profile-email">{data}</h3>
         <components.DoneRecipesButton />
         <components.FavoritesButton />
         <components.LogoutButton />
       </main>
+
       <components.Footer />
     </div>
   );

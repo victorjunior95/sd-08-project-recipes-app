a115dd102488787e416272122f46899bf7e70165
JRGCast <joao.ralph1@gmail.com>
2021-03-26 18:43:21 -0300

    adicionada logica a explorer foodIngredients, parei para fazer o pull do trabalho do Maycon

--- src/pages/Explorer/ExplorerFoodsIngredients.js
+++ src/pages/Explorer/ExplorerFoodsIngredients.js
@@ -2,20 +2,30 @@ import React, { useState, useEffect } from 'react';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 import { showCompleteLists } from '../../services/api';
+import { Card } from '../../components/Card';
 
 function ExplorerFoodsIngredients() {
+  const STOP_INDEX = 11;
   const [ingredientsList, setIngredientsList] = useState([]);
 
   useEffect(() => {
     async function requestNSet() {
       const requestList = await showCompleteLists('ingredients', 'Foods');
-      setIngredientsList(requestList);
+      setIngredientsList(requestList.meals);
     } requestNSet();
   }, []);
+
   return (
     <div>
       <Header name="Explorar Ingredientes" icon="true" />
-      {console.log(ingredientsList)}
+      <div className="cards">
+        {ingredientsList && ingredientsList.filter((ingredient, index) => index <= STOP_INDEX).map((item, index) => (<Card
+          key={ item.idIngredient }
+          id={ item.idIngredient }
+          name={ item.strIngredient }
+        />))}
+      </div>
+      {console.log(ingredientsList[0], ingredientsList[1], ingredientsList[2])}
       <Footer />
     </div>
   );

305a9b050dc137f65abbda9109dbeecbc8224df3
Rosiele David <rosieleserpa@hotmail.com>
2021-03-26 15:33:10 -0300

    [Group-4][Req-77] - refatorando FoodProvider

--- src/context/comidaContext/FoodProvider.js
+++ src/context/comidaContext/FoodProvider.js
@@ -1,33 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import FoodContext from './FoodContext';
-import {
-  requestMealRecipe,
-  requestFoodCategory,
-  requestMealsByCategory,
-} from '../../services/API';
+import { requestMealRecipe } from '../../services/API';
 
 function FoodProvider({ children }) {
   const [searchInput, setSearchInput] = useState('');
   const [searchType, setSearchType] = useState('');
+  const [foods, setFoods] = useState([]);
+
   const handleSearchInput = ({ target }) => setSearchInput(target.value);
   const handleSearchType = ({ target }) => setSearchType(target.value);
-  const [currentCategory, setCurrentCategory] = useState('');
-
-  const [foods, setFoods] = useState([]);
-  const [foodCategory, setFoodCategory] = useState([]);
-  const [filteredMeals, setFilteredMeals] = useState([]);
-  const handleFilteredMeals = async ({ target }) => {
-    const categoryName = target.innerText;
-    if (categoryName === 'All') return setFilteredMeals([]);
-    if (filteredMeals.length === 0 || currentCategory !== categoryName) {
-      setCurrentCategory(categoryName);
-      const result = await requestMealsByCategory(categoryName);
-      setFilteredMeals(result.meals);
-    } else {
-      setFilteredMeals([]);
-    }
-  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -40,26 +22,15 @@ function FoodProvider({ children }) {
   const [detailFoods, setDetailsFoods] = useState([]);
   const [recomendations, setRecomendations] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await requestFoodCategory();
-      setFoodCategory(result.meals);
-    };
-    fetchData();
-  }, []);
-
   const provide = {
     values: {
       searchInput,
       searchType,
-      filteredMeals,
       foods,
       detailFoods,
       recomendations,
-      foodCategory,
     },
     functions: {
-      handleFilteredMeals,
       handleSearchInput,
       handleSearchType,
       setFoods,

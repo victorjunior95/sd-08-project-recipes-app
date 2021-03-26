3312f323183519faa887c95c88c73c0d957220cc
Víctor Cançado <victor.cancado@gmail.com>
2021-03-26 18:40:12 -0300

    Req. 2 completo

--- src/pages/Bebidas.js
+++ src/pages/Bebidas.js
@@ -2,16 +2,17 @@ import React, { useContext, useState, useEffect } from 'react';
 import CardBebida from '../components/CardBebida';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
-import { getCategoryDrinks } from '../services/API';
+import { getCategoryDrinks, getDrinkCategory } from '../services/API';
 import RecipesContext from '../context/RecipesContext';
 
 export default function Bebidas() {
   const { data, drinkRandom } = useContext(RecipesContext);
-  const { drink } = data;
   const [card, setCard] = useState(false);
+  const [arrayOfDrinksCategories, setArrayOfDrinksCategories] = useState([]);
   const LIMITER = 12;
   const FIVE = 5;
   const [listDrinkCategories, setListDrinkCategories] = useState([]);
+
   useEffect(() => {
     const getListCategories = async () => {
       const listDrinkCategory = await getCategoryDrinks();
@@ -26,23 +27,43 @@ export default function Bebidas() {
     setCard(true);
   }, []);
 
+  const handleClickDrink = async ({ target: { value } }) => {
+    const drinkCatergories = await getDrinkCategory(value);
+    setArrayOfDrinksCategories(drinkCatergories);
+    setCard(true);
+  };
+
+  useEffect(() => {
+    setArrayOfDrinksCategories(data.drink);
+    setCard(true);
+  }, [data]);
+
   return (
     <div>
       <Header pageTitle="Bebidas" />
       <div>
+        <button
+          type="button"
+          data-testid="All-category-filter"
+          onClick={ data.drink }
+        >
+          All
+        </button>
         {
-          listDrinkCategories.map((item) => (
+          card && listDrinkCategories.map((item) => (
             <button
               type="button"
               key={ item.strCategory }
               data-testid={ `${item.strCategory}-category-filter` }
+              value={ item.strCategory }
+              onClick={ (e) => handleClickDrink(e) }
             >
               {item.strCategory}
             </button>))
         }
       </div>
       <section>
-        { card && drink.map((d, i) => {
+        { card && arrayOfDrinksCategories.map((d, i) => {
           const { idDrink } = d;
           return (i < LIMITER) && (
             <CardBebida

3831bc67b47d9bff9e6108ee6a49c9bf5da2e4aa
hugobrr <hugobrr@gmail.com>
2021-03-26 14:48:36 -0300

    Comit de troca

--- src/pages/ReceitasEmProgress.js
+++ src/pages/ReceitasEmProgress.js
@@ -62,9 +62,9 @@ const Detalhes = () => {
       <h3 data-testid="recipe-category">
         { isMeal ? foodDetails.strCategory : foodDetails.strAlcoholic}
       </h3>
-      <button type="button" onClick={ copyToClipBoard } data-testid="share-btn">
+      {/* <button type="button" onClick={ copyToClipBoard } data-testid="share-btn">
         <img src={ shareIcon } alt="Share" />
-      </button>
+      </button> */}
       <button
         type="button"
         data-testid="favorite-btn"

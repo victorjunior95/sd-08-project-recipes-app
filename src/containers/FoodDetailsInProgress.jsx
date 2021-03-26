492d45c67ed59ae0266b6c0d69057848cdaa5919
Bruno Martins <bruno.martins217@gmail.com>
2021-03-26 16:32:57 -0300

    Tela Receitas em Progresso

--- src/containers/FoodDetailsInProgress.jsx
+++ src/containers/FoodDetailsInProgress.jsx
@@ -1,14 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 const FoodDetailsInProgress = () => {
-  const [redirect, setRedirect] = useState(false);
-  useEffect(() => {
-    setRedirect(true);
-    return () => {
-      setRedirect(false);
-    };
-  }, [redirect]);
+  // const [redirect, setRedirect] = useState(false);
+  const history = useHistory();
+  const id = history.location.pathname;
+  const res = id.replace(/\D/g, ''); // https://stackoverflow.com/questions/30607419/return-only-numbers-from-string/30607466
+
+  // useEffect(() => {
+  //   setRedirect(true);
+  //   return () => {
+  //     setRedirect(false);
+  //   };
+  // }, [redirect]);
+
+  console.log(res);
   return (
+
     <div>
       Food Recipe In Progress
     </div>

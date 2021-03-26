1a56ce0dbdccfaae714dc430465f5a08d79a318c
gabrieldeori <deori.pessoal@gmail.com>
2021-03-26 11:03:46 -0300

    :package: Estrutura básica página

--- src/pages/InProgress/index.js
+++ src/pages/InProgress/index.js
@@ -1,9 +1,58 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { useIsMeal } from '../../services/customHooks';
+import { fetchDetails, fetchDrinks, fetchMeal } from '../../services/API';
+import { saveActualRecipe, saveRecommendations } from '../../redux/actions/details';
+import './inProgress.css';
 
-export default function InProgress() {
+import Loading from '../../components/Details/Loading';
+import CoverImg from '../../components/Details/CoverImg';
+import Title from '../../components/Details/Title';
+import ShareFavBtn from '../../components/Details/ShareFavBtn';
+import StepList from '../../components/InProgress/StepList';
+import InstructionsText from '../../components/Details/InstructionsText';
+
+async function getRecipe(dispatch, id, isMeal) {
+  let api = '';
+  let recommendations = '';
+  if (isMeal) {
+    api = 'meal';
+    recommendations = await fetchDrinks(null, 'firstFetch');
+  } else {
+    api = 'beverage';
+    recommendations = await fetchMeal(null, 'firstFetch');
+  }
+  dispatch(saveRecommendations(recommendations));
+  const actualRecipe = await fetchDetails(id, api);
+  dispatch(saveActualRecipe(actualRecipe));
+}
+
+function Details() {
+  const { actualRecipe } = useSelector((state) => state.detailsReducer);
+  const isMeal = useIsMeal();
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    getRecipe(dispatch, id, isMeal);
+  }, []);
+  if (Object.keys(actualRecipe).length < 1) {
+    return <Loading />;
+  }
   return (
-    <div>
-      in-progress
-    </div>
+    <main className="recipe-details">
+      <CoverImg />
+      <div className="upper-container">
+        <Title />
+        <ShareFavBtn />
+      </div>
+      <StepList />
+      <InstructionsText />
+      <button type="button" data-testid="finish-recipe-btn">
+        Finalizar Receita
+      </button>
+    </main>
   );
 }
+
+export default Details;

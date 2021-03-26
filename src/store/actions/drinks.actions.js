b8fc71593312643047c5ab73c20bd0140f853925
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-03-26 17:15:01 -0300

    Passa endpoint como template literal

--- src/store/actions/drinks.actions.js
+++ src/store/actions/drinks.actions.js
@@ -1,19 +1,22 @@
 export const fetchDrinks = () => (dispatch) => {
+  const ORDINARY_DRINKS = 'Ordinary Drink';
+  const MILK_FLOAT_SHAKE_DRINKS = 'Milk / Float / Shake';
+  const OTHER_UNKNOWN_DRINKS = 'Other/Unknown';
   fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=')
     .then((response) => response.json())
     .then((data) => dispatch({ type: 'FETCH_DRINKS', payload: data.drinks }));
-  fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink')
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${ORDINARY_DRINKS}`)
     .then((response) => response.json())
     .then((data) => dispatch({ type: 'FETCH_ORDINARY_DRINKS', payload: data.drinks }));
   fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail')
     .then((response) => response.json())
     .then((data) => dispatch({ type: 'FETCH_COCKTAIL_DRINKS', payload: data.drinks }));
-  fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Milk%20/%20float%20/%20shake')
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${MILK_FLOAT_SHAKE_DRINKS}`)
     .then((response) => response.json())
     .then((data) => dispatch(
       { type: 'FETCH_MILK_FLOAT_SHAKE_DRINKS', payload: data.drinks },
     ));
-  fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Other/unknown')
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${OTHER_UNKNOWN_DRINKS}`)
     .then((response) => response.json())
     .then((data) => dispatch(
       { type: 'FETCH_OTHER_UNKNOWN_DRINKS', payload: data.drinks },

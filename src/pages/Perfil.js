92395048f08eaf644b5d9eb523b3fd4ef836d686
v-icto-r <victor130496@gmail.com>
2021-03-26 12:23:45 -0300

    requisito do 82 ao 84

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -5,6 +5,31 @@ export default function Perfil() {
   return (
     <div>
       <Header pageTitle="Perfil" />
+      <h5 data-testid="profile-email">Aqui vem um E-mail</h5>
+      <button
+        className="g6-header-button"
+        type="button"
+        data-testid="profile-done-btn"
+      >
+        Receitas Feitas
+
+      </button>
+      <button
+        className="g6-header-button"
+        type="button"
+        data-testid="profile-favorite-btn"
+      >
+        Receitas Favoritas
+
+      </button>
+      <button
+        className="g6-header-button"
+        type="button"
+        data-testid="profile-logout-btn"
+      >
+        Sair
+
+      </button>
     </div>
   );
 }

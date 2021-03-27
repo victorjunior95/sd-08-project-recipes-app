647558f3b6151fa3edca239597286614bad1b3e7
JRGCast <joao.ralph1@gmail.com>
2021-03-26 22:22:20 -0300

    implementada pagina NotFound, req 81 OK

--- src/pages/Explorer/ExplorerFoodsRegion.js
+++ src/pages/Explorer/ExplorerFoodsRegion.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 
 function ExplorerFoodsRegion() {
+  const [areaList, setAreaList] = useState([]);
   return (
     <div>
       <Header name="Explorar Origem" icon="true" currentPage="Foods" />

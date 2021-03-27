625bd25a7fc872a73243055644a4aa042f0e166d
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-03-27 19:40:58 -0300

    Exclir comentarios

--- src/pages/Details/Details.js
+++ src/pages/Details/Details.js
@@ -27,7 +27,6 @@ const Details = ({ title, match, history }) => {
     console.log(drink);
     return drink;
   };
-
   useEffect(() => {
     (async () => {
       setLoad(true);
@@ -36,35 +35,6 @@ const Details = ({ title, match, history }) => {
       setLoad(false);
     })();
   }, []);
-
-  // Simulação do doneRecipes
-  // useEffect(() => {
-  //   const doneRecipes = (title === "Comidas")
-  //   ? [{
-  //     "id": "52771",
-  //     "type": "comida",
-  //     "area": "Italian",
-  //     "category": "Vegetarian",
-  //     "alcoholicOrNot": "",
-  //     "name": "Spicy Arrabiata Penne",
-  //     "image": "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg",
-  //     "doneDate": "22/6/2020",
-  //     "tags": ["Pasta", "Curry"]
-  //   }]
-  //   : [{
-  //     "id": "178319",
-  //     "type": "bebida",
-  //     "area": "",
-  //     "category": "Cocktail",
-  //     "alcoholicOrNot": "Alcoholic",
-  //     "name": "Aquamarine",
-  //     "image": "https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg",
-  //     "doneDate": "23/6/2020",
-  //     "tags": []
-  //   }];
-  //   localStorage.setItem('doneRecipes', JSON.stringify(doneRecipes));
-  // }, [])
-
   const handleClick = () => {
     setProductDetails({ object: object, isLoading: isLoading });
     if (title === 'Comidas') {
@@ -75,49 +45,54 @@ const Details = ({ title, match, history }) => {
   };
 
   const testInitializedRecipe = () => {
-    if (JSON.parse(localStorage.getItem("inProgressRecipes"))) {
-      const initializedRecipe = JSON.parse(localStorage.getItem("inProgressRecipes"))
-      const initializedList = (title === "Comidas") ? Object.keys(initializedRecipe.meals) : Object.keys(initializedRecipe.cocktails)
-      const isInitializedRecipe = initializedList.some(idInitialized => id === idInitialized);
-      return isInitializedRecipe
+    if (JSON.parse(localStorage.getItem('inProgressRecipes'))) {
+      const initializedRecipe = JSON.parse(
+        localStorage.getItem('inProgressRecipes'),
+      );
+      const initializedList =
+        title === 'Comidas'
+          ? Object.keys(initializedRecipe.meals)
+          : Object.keys(initializedRecipe.cocktails);
+      const isInitializedRecipe = initializedList.some(
+        (idInitialized) => id === idInitialized,
+      );
+      return isInitializedRecipe;
     }
-  }
+  };
 
   const testDoneRecipe = () => {
-    if (JSON.parse(localStorage.getItem("doneRecipes"))) {
-      const doneRecipes = JSON.parse(localStorage.getItem("doneRecipes"));
-      const isDoneRecipe = doneRecipes.some(recipe => id === recipe.id);
-      return isDoneRecipe
+    if (JSON.parse(localStorage.getItem('doneRecipes'))) {
+      const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
+      const isDoneRecipe = doneRecipes.some((recipe) => id === recipe.id);
+      return isDoneRecipe;
     }
-  }
+  };
 
   const renderButton = () => {
     if (!testDoneRecipe()) {
-      if(!testInitializedRecipe()){
+      if (!testInitializedRecipe()) {
         return (
           <Button
-          className="btn-recipe btn btn-primary w-100"
-          data-testid="start-recipe-btn"
-          onClick={handleClick}
+            className="btn-recipe btn btn-primary w-100"
+            data-testid="start-recipe-btn"
+            onClick={handleClick}
           >
             Iniciar receita
           </Button>
-        )
+        );
       } else {
         return (
           <Button
-          className="btn-recipe btn btn-primary w-100"
-          data-testid="start-recipe-btn"
-          onClick={handleClick}
+            className="btn-recipe btn btn-primary w-100"
+            data-testid="start-recipe-btn"
+            onClick={handleClick}
           >
             Continuar Receita
           </Button>
-        )
+        );
       }
-    } 
-  }
-
-  // De repente eu tenha que colocar o load aqui, por causa do botão
+    }
+  };
   return (
     <>
       <CardDetails title={title} object={object} isLoading={isLoading}>

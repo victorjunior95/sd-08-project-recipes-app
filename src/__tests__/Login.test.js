51c80b08f3c2442f16790f4414626586d709eb40
rafasysop <rafaelcostamoura@gmail.com>
2021-03-29 12:23:09 -0300

    mudando o comp app para Login no test

--- src/__tests__/Login.test.js
+++ src/__tests__/Login.test.js
@@ -3,6 +3,7 @@ import React from 'react';
 import renderWithRouter from '../routes/renderWithRouter';
 
 import App from '../App';
+import Login from '../pages/Login';
 
 const emailInput = 'email-input';
 const passwordInput = 'password-input';
@@ -16,37 +17,37 @@ describe('Testing page Login', () => {
   });
 
   test('should Exist Text Digite seu email:', () => {
-    const { getByTestId } = renderWithRouter(<App />);
+    const { getByTestId } = renderWithRouter(<Login />);
     const inputEmail = getByTestId(emailInput);
     expect(inputEmail.previousSibling.textContent).toBe('Digite seu email:');
   });
 
   test('should Exist Input Email', () => {
-    const { getByTestId } = renderWithRouter(<App />);
+    const { getByTestId } = renderWithRouter(<Login />);
     const inputEmail = getByTestId(emailInput);
     expect(inputEmail).toBeInTheDocument();
   });
 
   test('should Exist Text Digite sua Senha:', () => {
-    const { getByTestId } = renderWithRouter(<App />);
+    const { getByTestId } = renderWithRouter(<Login />);
     const inputPassword = getByTestId(passwordInput);
     expect(inputPassword.previousSibling.textContent).toBe('Digite sua Senha:');
   });
 
   test('should Exist Input Password', () => {
-    const { getByTestId } = renderWithRouter(<App />);
+    const { getByTestId } = renderWithRouter(<Login />);
     const inputPassword = getByTestId(passwordInput);
     expect(inputPassword).toBeInTheDocument();
   });
 
   test('should Exist Button Login', () => {
-    const { getByTestId } = renderWithRouter(<App />);
+    const { getByTestId } = renderWithRouter(<Login />);
     const btn = getByTestId(buttonLogin);
     expect(btn).toBeInTheDocument();
   });
 
   test('should Text Button Login to equal Entrar', () => {
-    const { getByTestId } = renderWithRouter(<App />);
+    const { getByTestId } = renderWithRouter(<Login />);
     const btn = getByTestId(buttonLogin);
     expect(btn.textContent).toBe('Entrar');
   });

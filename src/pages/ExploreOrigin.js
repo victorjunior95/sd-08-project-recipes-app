727495b0072a3c4b5b4429226dbfabd93534a8b7
Gustavo Thirion - Turma 8 <gustavo.fthirion@gmail.com>
2021-03-29 16:52:21 -0300

    falta tela  explorar origin

--- src/pages/ExploreOrigin.js
+++ src/pages/ExploreOrigin.js
@@ -1,9 +1,46 @@
 import React from 'react';
 
+const areas = [
+  { strArea: 'American' },
+  { strArea: 'British' },
+  { strArea: 'Canadian' },
+  { strArea: 'Chinese' },
+  { strArea: 'Dutch' },
+  { strArea: 'Egyptian' },
+  { strArea: 'French' },
+  { strArea: 'Greek' },
+  { strArea: 'Indian' },
+  { strArea: 'Irish' },
+  { strArea: 'Italian' },
+  { strArea: 'Jamaican' },
+  { strArea: 'Japanese' },
+  { strArea: 'Kenyan' },
+  { strArea: 'Malaysian' },
+  { strArea: 'Mexican' },
+  { strArea: 'Moroccan' },
+  { strArea: 'Russian' },
+  { strArea: 'Spanish' },
+  { strArea: 'Thai' },
+  { strArea: 'Tunisian' },
+  { strArea: 'Turkish' },
+  { strArea: 'Unknown' },
+  { strArea: 'Vietnamese' },
+];
+
 function ExploreOrigin() {
   return (
     <div>
       Explorar Origem
+      <select data-testid="explore-by-area-dropdown">
+        { areas.map(({ strArea }) => (
+          <option
+            data-testid="explore-by-area-dropdown"
+            key={ strArea }
+          >
+            { strArea }
+          </option>
+        )) }
+      </select>
     </div>
   );
 }

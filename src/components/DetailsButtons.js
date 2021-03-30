336b3ba32902e5b0d1ac27a36f0d578ebdcfd733
Thais Quintela <thais.s.quintela@gmail.com>
2021-03-30 18:09:22 -0300

    Implementa funçao de favoritar

--- src/components/DetailsButtons.js
+++ src/components/DetailsButtons.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { PropTypes } from 'prop-types';
 import { Link } from 'react-router-dom';
 
 function DetailsButtons({ route, id, page }) {
-  // https://stackoverflow.com/questions/16083919/push-json-objects-to-array-in-localstorage
+  const ids = JSON.parse(localStorage.getItem('inProgressRecipes')) || [];
+  console.log(ids);
+  // const { ids, setaIds } = useContext(Context);
+
+  useEffect(() => {
+    if (!Object.values(ids).includes(id)
+    && window.location.href === `/${route}/${id}/`) {
+      document.getElementById('start-recipe-btn').innerText = 'Iniciar Receita';
+    }
+  }, []);
+
   function SaveProgressRecipes(data) {
     let a = [];
     a = JSON.parse(localStorage.getItem('inProgressRecipes')) || [];
@@ -26,16 +36,18 @@ function DetailsButtons({ route, id, page }) {
             to={ `/${route}/${id}/in-progress` }
             className="last-btn"
             data-testid="start-recipe-btn"
-            id="start-recipe-btn"
             style={ { display: 'none' } }
+            id="start-recipe-btn"
             onClick={ () => {
+              ids.push(id);
+              localStorage.setItem('inProgressRecipes', JSON.stringify(ids));
               SaveProgressRecipes(id);
             } }
           >
-            { localStorage.getItem('inProgressRecipes')
-              && JSON.parse(localStorage.getItem('inProgressRecipes'))
-                .some((item) => item === id)
-              ? 'Continuar Receita' : 'Iniciar Receita' }
+            Continuar Receita
+            {/* { (Object.values(ids).includes(id))
+              ? 'Continuar Receita'
+              : 'Iniciar Receita' } */}
           </Link>
 
         ) : (
@@ -45,6 +57,7 @@ function DetailsButtons({ route, id, page }) {
             className="last-btn"
             data-testid="finish-recipe-btn"
             onClick={ () => {
+              localStorage.setItem('finishedRecipes', JSON.stringify(ids));
               SaveFinishedRecipes(id);
             } }
             // disabled={ document.querySelectorAll('.checkbox').checked }
--- src/pages/DetalhesComida.js
+++ src/pages/DetalhesComida.js
@@ -4,18 +4,18 @@ import Context from '../context/Context';
 import RecipeDetails from '../components/RecipeDetails';
 import DetailsButtons from '../components/DetailsButtons';
 
+const ids = JSON.parse(localStorage.getItem('finishedRecipes')) || [];
+
 function DetalhesComida({ match }) {
   const { requestRecipeDetails } = useContext(Context);
   const { id } = match.params;
 
-  // const ids = JSON.parse(localStorage.getItem('finishedRecipes')) || [];
-
   useEffect(() => {
     requestRecipeDetails('themealdb', id, 'thecocktaildb');
     document.getElementById('start-recipe-btn').style.display = 'block';
-    // if (ids.includes(id)) {
-    //   document.getElementById('start-recipe-btn').style.display = 'none';
-    // }
+    if (ids.includes(id)) {
+      document.getElementById('start-recipe-btn').style.display = 'none';
+    }
   }, []);
 
   return (

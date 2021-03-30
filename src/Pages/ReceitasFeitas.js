ed73ea3ed8556895485c19365f0a3f3101d813d3
Douglas Ferreira <dodofp.df@gmail.com>
2021-03-30 15:28:54 -0300

    Requisito 58

--- src/Pages/ReceitasFeitas.js
+++ src/Pages/ReceitasFeitas.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CardFeitas from '../components/CardFeitas';
 
 import '../styles/ReceitasFeitas.css';
 
 function ReceitasFeitas() {
+  const [filterType, setFilterType] = useState('All');
+
   const verify = () => {
+    let filtered = [];
     if (localStorage.getItem('doneRecipes') !== null) {
       const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
+      if (filterType !== 'All') {
+        filtered = doneRecipes.filter((elem) => elem.type === filterType);
+      } else {
+        filtered = doneRecipes;
+      }
       return (
-        doneRecipes.map((elem, index) => (
+        filtered.map((elem, index) => (
           <CardFeitas key={ index } objDetail={ elem } index={ index } />
         ))
       );
     }
     return <span>Você não tem receitas feitas</span>;
+  };
+
+  const handleClick = (type) => {
+    setFilterType(type);
   };
 
   return (
@@ -26,18 +38,22 @@ function ReceitasFeitas() {
           <button
             type="button"
             data-testid="filter-by-all-btn"
+            onClick={ () => handleClick('All') }
           >
             All
           </button>
           <button
             type="button"
             data-testid="filter-by-food-btn"
+            onClick={ () => handleClick('comida') }
           >
             Food
           </button>
           <button
             type="button"
             data-testid="filter-by-drink-btn"
+            onClick={ () => handleClick('bebida') }
+
           >
             Drinks
           </button>

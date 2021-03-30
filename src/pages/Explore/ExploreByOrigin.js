1a93989b7f8f3d3b84c056958bdb527e5fc50667
Felipe Segovia <felipe.vsegovia@gmail.com>
2021-03-30 16:20:00 -0300

    Explore Ingredients  screen - req75-77(fixed-2)

--- src/pages/Explore/ExploreByOrigin.js
+++ src/pages/Explore/ExploreByOrigin.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import { Header, Footer, RecipeCards } from '../../component';
 
 export default function ExploreByOrigin() {

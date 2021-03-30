faca47709a55147f27ca7edd2f27f5704bd09367
Paulo Simões <paulohbsimoes@gmail.com>
2021-03-30 15:32:44 -0300

    Corrige linter

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import throttle from 'lodash/throttle';
 import rootReducer from './ducks';
 import * as storage from '../services/storage';
 

85c3bdf7fb5252a6d8aa03c94eb52d4e7969c8d3
rafasysop <rafaelcostamoura@gmail.com>
2021-03-31 15:39:15 -0300

    resolvendo teste foods

--- src/__tests__/Food.test.js
+++ src/__tests__/Food.test.js
@@ -1,39 +1,33 @@
-// import React from 'react';
-// import renderWithRouter from '../routes/renderWithRouter';
+import React from 'react';
+import renderWithRouter from '../routes/renderWithRouter';
 
-// import ContextFood from '../context/ContextFood';
+import ContextFood from '../context/ContextFood';
 
-// import Food from '../pages/Foods/Foods';
+import Food from '../pages/Foods/Foods';
 
-// const setup = () => {
-//   const utils = renderWithRouter(
-//     <ContextFood>
-//       <Food />
-//     </ContextFood>,
-//   );
-//   const header = utils.getByRole('header');
-//   const title = utils.getByTestId('page-title');
-//   const searchIcon = utils.getByTestId('search-top-btn');
-//   const categoryDiv = utils.getByTestId('All-category-filter');
+const setup = () => {
+  const utils = renderWithRouter(
+    <ContextFood>
+      <Food location={ {} } />
+    </ContextFood>,
+  );
+  const header = utils.getByRole('banner');
+  const title = utils.getByTestId('page-title');
+  const searchIcon = utils.getByTestId('search-top-btn');
+  const categoryDiv = utils.getByTestId('All-category-filter');
 
-//   return {
-//     header,
-//     title,
-//     categoryDiv,
-//     searchIcon,
-//     ...utils,
-//   };
-// };
+  return {
+    header,
+    title,
+    categoryDiv,
+    searchIcon,
+    ...utils,
+  };
+};
 
-// describe.only('Test Page Food', () => {
-//   it('should have header', () => {
-//     const { header } = setup();
-//     expect(header).toBeInTheDocument();
-//   });
-// });
-
-describe('Não consegui fazer o teste ainda', () => {
-  it('test', () => {
-
+describe('Test Page Food', () => {
+  it('should have header', () => {
+    const { header } = setup();
+    expect(header).toBeInTheDocument();
   });
 });

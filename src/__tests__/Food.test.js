b91ac7cabb99973fe0b32fa0a2ab0611905099d8
rafasysop <rafaelcostamoura@gmail.com>
2021-03-31 10:12:25 -0300

    Iniciando testes no foods

---
+++ src/__tests__/Food.test.js
@@ -0,0 +1,39 @@
+// import React from 'react';
+// import renderWithRouter from '../routes/renderWithRouter';
+
+// import ContextFood from '../context/ContextFood';
+
+// import Food from '../pages/Foods/Foods';
+
+// const setup = () => {
+//   const utils = renderWithRouter(
+//     <ContextFood>
+//       <Food />
+//     </ContextFood>,
+//   );
+//   const header = utils.getByRole('header');
+//   const title = utils.getByTestId('page-title');
+//   const searchIcon = utils.getByTestId('search-top-btn');
+//   const categoryDiv = utils.getByTestId('All-category-filter');
+
+//   return {
+//     header,
+//     title,
+//     categoryDiv,
+//     searchIcon,
+//     ...utils,
+//   };
+// };
+
+// describe.only('Test Page Food', () => {
+//   it('should have header', () => {
+//     const { header } = setup();
+//     expect(header).toBeInTheDocument();
+//   });
+// });
+
+describe('Não consegui fazer o teste ainda', () => {
+  it('test', () => {
+
+  });
+});

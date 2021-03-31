8554601d35d77a31149ca8841f62250c3ec1e3fc
Thiago Ferreira <thiagocristhian@gmail.com>
2021-03-31 13:18:00 -0300

    REQUISITO 1 - Ajustando renderWithRouter com o provider

--- src/renderWithRouter.js
+++ src/renderWithRouter.js
@@ -2,11 +2,21 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
+import ProviderRecipes from './context/ProviderRecipes';
 
 const renderWithRouter = (component) => {
   const history = createMemoryHistory();
   return ({
-    ...render(<Router history={ history }>{component}</Router>), history,
+    ...render(
+      <ProviderRecipes>
+        <Router
+          history={ history }
+        >
+          {component}
+        </Router>
+      </ProviderRecipes>,
+    ),
+    history,
   });
 };
 

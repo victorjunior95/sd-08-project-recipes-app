8508713cfd39e3f03ab606884b3f04d9b440f18c
rafasysop <rafaelcostamoura@gmail.com>
2021-04-01 07:55:07 -0300

    concertando Header, Iniciando Receitas Favoritas

--- src/pages/Profile/FavRecipes.js
+++ src/pages/Profile/FavRecipes.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from '../../components/Header';
 
 function FavRecipes() {
+  const favorites = localStorage.getItem('favoriteRecipes');
+  console.log(favorites);
   return (
     <div>
       <Header name="Receitas Favoritas" icon="false" />

312d4c3fc3b350cef175c3021a3291513e490ec4
JRGCast <joao.ralph1@gmail.com>
2021-04-01 12:03:08 -0300

    wtf esse datatestId NUNCA encontrado?

--- src/components/Card/InProgressCard.js
+++ src/components/Card/InProgressCard.js
@@ -2,9 +2,8 @@ import React, { useEffect, useState } from 'react';
 import './InProgressCard.css';
 import PropTypes from 'prop-types';
 
-const InProgressCard = (props) => {
-  const { path,
-    id, category, title, img, ingredients, alcohol, instructions } = props;
+const InProgressCard = ({ path,
+  id, category, title, img, ingredients, alcohol, instructions }) => {
   const [isDrinkOrFood, setIsDrinkOrFood] = useState('');
 
   useEffect(() => {
@@ -29,19 +28,57 @@ const InProgressCard = (props) => {
       </li>);
   });
 
-  const ingredientsMapping = ingredients.map((ingredient, index) => (
-    <div key={ ingredient }>
-      <input
-        id={ `id-${index}` }
-        type="checkbox"
-        data-testid="ingredient-step"
-        value={ ingredient }
-        // onChange={ ({ target }) => { console.log(target.checked); } }
-      />
-      <label htmlFor={ `id-${index}` }>
-        {ingredient}
-      </label>
-    </div>));
+  const ingredientsMapping = ingredients.map((ingredient, index) => {
+    if (ingredient !== '  ') {
+      return (
+        <div key={ `${index}-${ingredient}` }>
+          <input
+            id={ `id-${index}` }
+            type="checkbox"
+            data-testid="ingredient-step"
+            value={ ingredient }
+            // onChange={ ({ target }) => { console.log(target.checked); } }
+          />
+          <label htmlFor={ `id-${index}` }>
+            {ingredient}
+          </label>
+        </div>
+      );
+    }
+  });
+
+  const renderFood = () => (
+    <div
+      name={ id }
+      className="card-container"
+      data-testid="recipe-card"
+      role="button"
+    >
+      <h4 data-testid="recipe-title">{title}</h4>
+      <span data-testid="recipe-category">{`Category => ${category}`}</span>
+      <img src={ img } alt={ title } data-testid="recipe-photo" />
+      <ul>
+        {ingredientsMapping}
+      </ul>
+      <ol
+        style={
+          { display:
+          'flex',
+          flexFlow: 'column wrap',
+          textAlign: 'center',
+          listStylePosition: 'inside' }
+        }
+        data-testid="instructions"
+      >
+        {instructionsMapping}
+        Voilá!
+      </ol>
+      <div>
+        <button type="button" data-testid="favorite-btn">Favoritar</button>
+        <button type="button" data-testid="share-btn">Compartilhar</button>
+        <button type="button" data-testid="finish-recipe-btn">Finalizar</button>
+      </div>
+    </div>);
 
   const renderDrink = () => (
     <div
@@ -79,7 +116,8 @@ const InProgressCard = (props) => {
 
   return (
     <main>
-      {isDrinkOrFood === 'Drink' ? renderDrink() : ''}
+      {isDrinkOrFood === 'Drink' ? renderDrink() : renderFood()}
+      {console.log(ingredients)}
     </main>
   );
 };
@@ -91,12 +129,12 @@ InProgressCard.propTypes = {
   title: PropTypes.string.isRequired,
   img: PropTypes.string.isRequired,
   ingredients: PropTypes.arrayOf(PropTypes.string).isRequired,
-  alcohol: PropTypes.string.isRequired,
+  alcohol: PropTypes.string,
   instructions: PropTypes.string.isRequired,
 };
 
-// InProgressCard.defaultProps = {
-//   alcohol: PropTypes.string,
-// };
+InProgressCard.defaultProps = {
+  alcohol: PropTypes.string,
+};
 
 export default InProgressCard;
--- src/pages/Foods/FoodInProgress.js
+++ src/pages/Foods/FoodInProgress.js
@@ -1,11 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import InProgressCard from '../../components/Card/InProgressCard';
+import { getFoodFiltredById } from '../../services/api';
 
-function FoodInProgress() {
+function FoodInProgress(props) {
+  const { match: { path, params: { id } } } = props;
+  const [filteredById, setFilteredById] = useState('');
+  const [ingredientsAndMeasuresList, setIngredientsAndMeasuresList] = useState([]);
+  const isEmpty = (obj) => Object.keys(obj).length === 0; // verifica se o objeto está vazio;
+
+  function createIngredientList(receita) {
+    const ING_INDEX = 15;
+    let ingredientList = [];
+    let quantitiesList = [];
+    for (let i = 1; i <= ING_INDEX; i += 1) {
+      ingredientList = [...ingredientList, receita[`strIngredient${i}`]];
+      quantitiesList = [...quantitiesList, receita[`strMeasure${i}`]];
+    }
+    const ingredientAndMeasure = quantitiesList
+      .filter((qua) => qua !== null && qua !== '')
+      .map((mes, index) => `${mes} ${ingredientList[index]}`);
+    return setIngredientsAndMeasuresList(ingredientAndMeasure);
+  }
+
+  useEffect(() => {
+    const requestingAPI = async () => {
+      const fetchById = await getFoodFiltredById(id);
+      setFilteredById(fetchById);
+    }; requestingAPI();
+  }, [id]);
+
+  useEffect(() => {
+    if (!isEmpty(filteredById)) {
+      console.log('IS NOT EMPTY');
+      createIngredientList(filteredById);
+    }
+  }, [filteredById]);
+
+  const { strCategory,
+    strMeal, strMealThumb, strInstructions } = filteredById;
   return (
     <div>
-      Foods in Progress
+      {console.log(filteredById)}
+      {!isEmpty(filteredById) ? <InProgressCard
+        path={ path }
+        id={ id }
+        category={ strCategory }
+        title={ strMeal }
+        img={ strMealThumb }
+        ingredients={ ingredientsAndMeasuresList }
+        // alcohol={ strAlcoholic }
+        instructions={ strInstructions }
+      /> : <h1>Carregando comida...</h1> }
+
     </div>
   );
 }
+
+FoodInProgress.propTypes = {
+  match: PropTypes.shape({
+    path: PropTypes.string,
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default FoodInProgress;

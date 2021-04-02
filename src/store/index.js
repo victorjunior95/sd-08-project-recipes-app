2d8dc7d24e149f7b3c8d5def1595e9b06a6a3ef8
Paulo Simões <paulohbsimoes@gmail.com>
2021-04-02 11:12:36 -0300

    Remove setup inicial de doneRecipes usado em testes

--- src/store/index.js
+++ src/store/index.js
@@ -14,31 +14,6 @@ const INITIAL_IN_PROGRESS_STATE = {
   cocktails: {},
 };
 
-const doneRecipes = [
-  {
-    id: '52771',
-    type: 'comida',
-    area: 'Italian',
-    category: 'Vegetarian',
-    alcoholicOrNot: '',
-    name: 'Spicy Arrabiata Penne',
-    image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
-    doneDate: '23/06/2020',
-    tags: ['Pasta', 'Curry'],
-  },
-  {
-    id: '178319',
-    type: 'bebida',
-    area: '',
-    category: 'Cocktail',
-    alcoholicOrNot: 'Alcoholic',
-    name: 'Aquamarine',
-    image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
-    doneDate: '23/06/2020',
-    tags: [],
-  },
-];
-
 const INITIAL_STATE = {
   user: {
     ...INITIAL_USER_STATE,
@@ -53,8 +28,7 @@ const INITIAL_STATE = {
   },
 
   doneRecipes: {
-    // doneRecipes: JSON.parse(storage.getDoneRecipes()) || [],
-    doneRecipes,
+    doneRecipes: JSON.parse(storage.getDoneRecipes()) || [],
   },
 };
 

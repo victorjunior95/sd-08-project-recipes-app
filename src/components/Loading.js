a32702d9692ea9636f2b45837cea524af018a4b7
Ediberto Oliveira <edibertooliveira@aol.com>
2021-04-04 18:05:44 -0300

    fix: refactoey loading

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import '../styles/components/Loading.css';
 
 class Loading extends Component {
   render() {
+    const { state, children } = this.props;
+    if (!state) {
+      return children;
+    }
     return (
-      <div>
-        Homens Trabalhando Muito.......
+      <div className="containerLoading">
+        <span>Homens Trabalhando Muito.......</span>
       </div>
     );
   }
 }
 
-// Drinks.propTypes = {
-
-// };
+Loading.propTypes = {
+  state: PropTypes.bool.isRequired,
+  children: PropTypes.node.isRequired,
+};
 
 export default Loading;

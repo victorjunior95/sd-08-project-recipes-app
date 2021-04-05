629e94d71b87a8f5e6ae33c84eccbd43c5e57b6f
Thalys Matias Carrara <thalysmatiascarrara@gmail.com>
2021-04-05 18:17:17 -0400

    adicionando renderWithRouter com provider e tirando BrowserRouter do componente Routes para o index.js

--- src/Routes.js
+++ src/Routes.js
@@ -1,57 +1,55 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import containers from './containers/index';
 
 const Routes = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={ containers.Login } />
-      <Route exact path="/comidas" component={ containers.MainFood } />
-      <Route exact path="/comidas/:id" component={ containers.FoodDetails } />
-      <Route
-        exact
-        path="/comidas/:id/in-progress"
-        component={ containers.FoodDetailsInProgress }
-      />
-      <Route
-        exact
-        path="/bebidas"
-        component={ containers.Drinks }
-      />
-      <Route exact path="/bebidas/:id" component={ containers.DrinkDetails } />
-      <Route
-        exact
-        path="/bebidas/:id/in-progress"
-        component={ containers.DrinkDetainsInProgress }
-      />
-      <Route exact path="/explorar" component={ containers.Explore } />
-      <Route exact path="/perfil" component={ containers.Profile } />
-      <Route exact path="/receitas-feitas" component={ containers.DoneRecipes } />
-      <Route exact path="/receitas-favoritas" component={ containers.FavoriteRecipes } />
-      <Route exact path="/explorar/comidas" component={ containers.ExploreFoods } />
-      <Route exact path="/explorar/bebidas" component={ containers.ExploreDrinks } />
-      <Route
-        exact
-        path="/explorar/comidas/ingredientes"
-        component={ containers.ExploreFoodsByIngredients }
-      />
-      <Route
-        exact
-        path="/explorar/bebidas/ingredientes"
-        component={ containers.ExploreDrinksByIngredients }
-      />
-      <Route
-        exact
-        path="/explorar/bebidas/area"
-        component={ containers.NotFound }
-      />
-      <Route
-        exact
-        path="/explorar/comidas/area"
-        component={ containers.ExploreOrigination }
-      />
-    </Switch>
-  </BrowserRouter>
+  <Switch>
+    <Route exact path="/" component={ containers.Login } />
+    <Route exact path="/comidas" component={ containers.MainFood } />
+    <Route exact path="/comidas/:id" component={ containers.FoodDetails } />
+    <Route
+      exact
+      path="/comidas/:id/in-progress"
+      component={ containers.FoodDetailsInProgress }
+    />
+    <Route
+      exact
+      path="/bebidas"
+      component={ containers.Drinks }
+    />
+    <Route exact path="/bebidas/:id" component={ containers.DrinkDetails } />
+    <Route
+      exact
+      path="/bebidas/:id/in-progress"
+      component={ containers.DrinkDetainsInProgress }
+    />
+    <Route exact path="/explorar" component={ containers.Explore } />
+    <Route exact path="/perfil" component={ containers.Profile } />
+    <Route exact path="/receitas-feitas" component={ containers.DoneRecipes } />
+    <Route exact path="/receitas-favoritas" component={ containers.FavoriteRecipes } />
+    <Route exact path="/explorar/comidas" component={ containers.ExploreFoods } />
+    <Route exact path="/explorar/bebidas" component={ containers.ExploreDrinks } />
+    <Route
+      exact
+      path="/explorar/comidas/ingredientes"
+      component={ containers.ExploreFoodsByIngredients }
+    />
+    <Route
+      exact
+      path="/explorar/bebidas/ingredientes"
+      component={ containers.ExploreDrinksByIngredients }
+    />
+    <Route
+      exact
+      path="/explorar/bebidas/area"
+      component={ containers.NotFound }
+    />
+    <Route
+      exact
+      path="/explorar/comidas/area"
+      component={ containers.ExploreOrigination }
+    />
+  </Switch>
 );
 
 // Tela de detalhes de uma receita de comida: /comidas/{id-da-receita};

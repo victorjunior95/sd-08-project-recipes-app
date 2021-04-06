d428132d55f82e02c8d7c1de8550d135ced09b26
MarceloBorges <melombm@hotmail.com>
2021-04-05 21:45:08 -0500

    requisito 75 e 76

--- src/pages/ExploreDrinksByIngredient/index.js
+++ src/pages/ExploreDrinksByIngredient/index.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Card } from 'react-bootstrap';
 import ContainerDefault from '../../components/ContainerDefault';
+import { fetchIngredientDrinks, imgIngredienteDrink } from '../../services/theCockTailDB';
 
 // import { Container } from './styles';
 
 function ExploreDrinksByIngredient() {
+  const [useingredient, setIngredient] = useState([{}]);
+  console.log(fetchIngredientDrinks());
+  useEffect(() => {
+    const apiIngredient = async () => {
+      const ingred = await fetchIngredientDrinks();
+      setIngredient(ingred);
+    };
+    apiIngredient();
+  }, []);
+  const numberIngredient = 12;
+
   return (
     <ContainerDefault title="Explorar Ingredientes">
-      <p>Explorar Ingredientes Bebidas</p>
+      <div className="d-flex flex-wrap">
+        {useingredient.map((ingredient, index) => (index < numberIngredient
+
+          ? <Card
+              data-testid={ `${index}-ingredient-card` }
+              key={ index }
+          >
+            <Card.Img
+              data-testid={ `${index}-card-img` }
+              alt={ ingredient.strIngredient1 }
+              src={ imgIngredienteDrink(ingredient.strIngredient1) }
+            />
+            <Card.Title
+              data-testid={ `${index}-card-name` }
+            >
+              {ingredient.strIngredient1}
+            </Card.Title>
+            </Card> : null
+        ))}
+      </div>
     </ContainerDefault>
   );
 }

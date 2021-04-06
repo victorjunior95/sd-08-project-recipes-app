fc33f6ee50b0661a2de9124c4a27748639bc160f
Jorge Alves <alvesdjorge@gmail.com>
2021-04-06 17:52:30 -0300

    itens 60 a 66

--- src/pages/user/FavoriteRecipes.js
+++ src/pages/user/FavoriteRecipes.js
@@ -1,10 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
+import CardFavorites from '../../components/CardFavorites';
 import Header from '../../components/Header';
 
 export default function FavoriteRecipes() {
+  const [filter, setFilter] = useState('');
+  const [, forceUpdate] = useState('');
+  const favorites = JSON.parse(localStorage.getItem('favoriteRecipes'));
+  console.log(favorites);
+
+  const removeFavorite = (idx, id) => {
+    console.log(idx);
+    const newLocalStorage = favorites.filter((_, index) => index !== idx);
+    localStorage.setItem('favoriteRecipes', JSON.stringify(newLocalStorage));
+    forceUpdate(id);
+  };
+
   return (
     <div>
-      <Header title="Receitas Favoritas" search="false" />
+      <Header
+        name="Receitas Favoritas"
+        currentPage="Favorites"
+        icon="false"
+      />
+      <div className="filter-btn">
+        <button
+          data-testid="filter-by-all-btn"
+          type="button"
+          onClick={ () => setFilter('') }
+        >
+          All
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-food-btn"
+          onClick={ () => setFilter('comida') }
+        >
+          Comidas
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-drink-btn"
+          onClick={ () => setFilter('bebida') }
+        >
+          Bebidas
+        </button>
+      </div>
+      <main>
+        {
+          favorites
+            .filter((item) => {
+              if (filter === '') return item;
+              return item.type === filter;
+            })
+            .map((card, index) => {
+              console.log('o item map', card);
+              console.log('com index', index);
+              return (
+                <CardFavorites
+                  key={ card.id }
+                  index={ index }
+                  img={ card.image }
+                  id={ card.id }
+                  removeFavorite={ removeFavorite }
+                  title={ card.name }
+                  alt={ card.name }
+                  type={ card.type }
+                  desc={ card.type === 'bebida'
+                    ? card.alcoholicOrNot
+                    : `${card.area} - ${card.category}` }
+                />
+              );
+            })
+        }
+      </main>
     </div>
   );
 }

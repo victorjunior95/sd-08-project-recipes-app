47046c7a35a259359a509d9a775eb6a887ddacac
Fernanda PC <nandateixmarques@gmail.com>
2021-04-06 19:23:38 -0300

    adicionando os teste na tela explorar

--- src/__tests__/profile.test.js
+++ src/__tests__/profile.test.js
@@ -4,35 +4,35 @@ import renderWithRouter from './helpers/renderWithRouter';
 import Profile from '../containers/Profile';
 import App from '../App';
 
-describe('Os elementos da página de perfil', () => {
-  it('Mostra a tela de perfil quando a rota é `/perfil`', () => {
+describe('The elements of the profile page', () => {
+  it('Shows the profile screen when the route is `/perfil`', () => {
     const { history } = renderWithRouter(<App />);
     history.push('/perfil');
     expect(history.location.pathname).toBe('/perfil');
   });
-  it('existe um cabeçalho no componente', () => {
+  it('should have a header in the component', () => {
     const { getByTestId } = renderWithRouter(<Profile />);
     const headerContainer = getByTestId('header-container');
     expect(headerContainer).toBeInTheDocument();
   });
-  it('existe um botão de perfil no cabeçalho', () => {
+  it('should have a profile button in the header', () => {
     const { getByTestId } = renderWithRouter(<Profile />);
     const profileButton = getByTestId('profile-top-btn');
     expect(profileButton).toBeInTheDocument();
   });
-  it('existe um título no cabeçalho', () => {
+  it('should have a title in the header', () => {
     const { getByTestId, getByRole } = renderWithRouter(<Profile />);
     const pageTitle = getByTestId('page-title');
     const profilePage = getByRole('heading', { name: /perfil/i });
     expect(pageTitle).toBeInTheDocument();
     expect(profilePage).toBeInTheDocument();
   });
-  it('existe um botão endereço de email do perfil', () => {
+  it('should have a profile email address button', () => {
     const { getByTestId } = renderWithRouter(<Profile />);
     const emailProfile = getByTestId('profile-email');
     expect(emailProfile).toBeInTheDocument();
   });
-  it('existe um botão Receitas Feitas, Receitas Favoritas e Sair', () => {
+  it('should have a button Receitas Feitas, Receitas Favoritas and Sair', () => {
     const { getByTestId } = renderWithRouter(<Profile />);
     const buttonRecipesDone = getByTestId('profile-done-btn');
     const buttonFavoriteRecipes = getByTestId('profile-favorite-btn');
@@ -41,40 +41,40 @@ describe('Os elementos da página de perfil', () => {
     expect(buttonFavoriteRecipes).toBeInTheDocument();
     expect(buttonLogout).toBeInTheDocument();
   });
-  it('ao clicar no botão Receitas Feitas é direcionada para a página', () => {
+  it('clicking on the `Receitas Feitas` button takes you to the page', () => {
     const { history, getByText } = renderWithRouter(<Profile />);
     userEvent.click(getByText(/receitas feitas/i));
     const { pathname } = history.location;
     expect(pathname).toBe('/receitas-feitas');
   });
-  it('ao clicar no botão Receitas Favoritas é direcionada para página', () => {
+  it('clicking on the `Receitas Favoritas` takes you to the page', () => {
     const { history, getByText } = renderWithRouter(<Profile />);
     userEvent.click(getByText(/receitas favoritas/i));
     const { pathname } = history.location;
     expect(pathname).toBe('/receitas-favoritas');
   });
-  it('ao clicar no botão Sair é direcionada para página de Login', () => {
+  it('clicking the `Sair` button takes you to the Login page', () => {
     const { history, getByText } = renderWithRouter(<Profile />);
     userEvent.click(getByText(/sair/i));
     const { pathname } = history.location;
     expect(pathname).toBe('/');
   });
-  it('existe um rodapé no componente', () => {
+  it('should have a footer on the component', () => {
     const { getByTestId } = renderWithRouter(<Profile />);
     const footer = getByTestId('footer');
     expect(footer).toBeInTheDocument();
   });
-  it('existe uma imagem de drink que leva para as receitas de drinks', () => {
+  it('should have a drink image that leads to the drink recipes', () => {
     const { getByTestId } = renderWithRouter(<Profile />);
     const buttonDrinks = getByTestId('drinks-bottom-btn');
     expect(buttonDrinks).toBeInTheDocument();
   });
-  it('existe uma imagem que leva para página de explorar receitas', () => {
+  it('should have an image that leads to the page to explore recipes', () => {
     const { getByTestId } = renderWithRouter(<Profile />);
     const ButtonExplore = getByTestId('explore-bottom-btn');
     expect(ButtonExplore).toBeInTheDocument();
   });
-  it('existe uma imagem que leva para página de comidas', () => {
+  it('there should be an image that leads to the food page', () => {
     const { getByTestId } = renderWithRouter(<Profile />);
     const ButtonFood = getByTestId('food-bottom-btn');
     expect(ButtonFood).toBeInTheDocument();

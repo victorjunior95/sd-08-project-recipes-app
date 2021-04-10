1e0662e04b0f0578b7298c67f0f95cfd3475bc2e
BrunoPDRS <bgpedrosas@gmail.com>
2021-04-10 19:55:38 -0300

    Testes FeitasFavoritas

---
+++ src/__tests__/FeitasFavoritas.spec.js
@@ -0,0 +1,141 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import copy from 'clipboard-copy';
+jest.mock('clipboard-copy');
+
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+
+import FeitasFavoritas from '../pages/FeitasFavoritas';
+import favoriteRecipes from '../mocks/favoriteRecipes';
+
+import fetchMock from '../mocks/fetch';
+import doneRecipes from '../mocks/doneRecipes';
+import shareIcon from '../images/shareIcon.svg';
+import { createBrowserHistory } from 'history';
+
+const renderPageHistory = (initialEntries) => {
+  const history = createMemoryHistory({ initialEntries });
+  return render(
+      <Router history={ history }>
+        <FeitasFavoritas />
+      </Router>
+    );
+};
+
+describe('Feitas', () => {
+  beforeEach(() => {
+    fetchMock();
+    Object.defineProperty(window, 'localStorage', {
+      value: {
+        getItem: jest.fn(() => JSON.stringify(doneRecipes)),
+        setItem: jest.fn(() => null),
+        clear: jest.fn(() => null),
+      },
+      writable: true,
+    });
+  });
+
+  test('54 - Implemente os elementos da tela de receitas feitas respeitando os atributos descritos no protótipo', async () => {
+    const {findByTestId} = renderPageHistory(['/receitas-feitas']);
+    expect(localStorage.getItem).toHaveBeenCalledWith('doneRecipes');
+    const allBtn = await findByTestId('filter-by-all-btn');
+    expect(allBtn.value).toBe('All');
+    const foodBtn = await findByTestId('filter-by-food-btn');
+    expect(foodBtn.value).toBe('Food');
+    const drinkBtn = await findByTestId('filter-by-drink-btn');
+    expect(drinkBtn.value).toBe('Drinks');
+
+    const firstRecipe = doneRecipes[0];
+    const firstRecipeImg = await findByTestId('0-horizontal-image');
+    expect(firstRecipeImg.src).toBe(firstRecipe.image);
+    const firstRecipeText = await findByTestId('0-horizontal-top-text');
+    expect(firstRecipeText.innerHTML)
+      .toBe(`${firstRecipe.area} - ${firstRecipe.category}`);
+    const firstRecipeName = await findByTestId('0-horizontal-name');
+    expect(firstRecipeName.innerHTML).toBe(firstRecipe.name);
+    const firstRecipeDate = await findByTestId('0-horizontal-done-date');
+    expect(firstRecipeDate.innerHTML).toBe(firstRecipe.doneDate);
+    const firstRecipeShare = await findByTestId('0-horizontal-share-btn');
+    expect(firstRecipeShare.src).toBe(shareIcon);
+    for (const tag of doneRecipes[0].tags) {
+      const firstRecipeTag = await findByTestId(`0-${tag}-horizontal-tag`);
+      expect(firstRecipeTag.innerHTML).toBe(tag);
+    };
+  });
+
+  test('55 e 56', async () => {
+    const {findByTestId} = renderPageHistory(['/receitas-feitas']);
+    const foodBtn = await findByTestId('filter-by-food-btn');
+    const drinkBtn = await findByTestId('filter-by-drink-btn');
+    userEvent.click(foodBtn);
+    const {area, category} = doneRecipes[0];
+    const firstFoodRecipeText = await findByTestId('0-horizontal-top-text');
+    expect(firstFoodRecipeText.innerHTML).toBe(`${area} - ${category}`);
+    userEvent.click(drinkBtn);
+    const {alcoholicOrNot} = doneRecipes[1];
+    const firstDrinkRecipeText = await findByTestId('0-horizontal-top-text');
+    expect(firstDrinkRecipeText.innerHTML).toBe(alcoholicOrNot);
+  });
+
+  test('57 - Desenvolva a solução de maneira que o botão de compartilhar deve copiar a URL da tela de detalhes da receita para o clipboard', async () => {
+    const {findByTestId, queryAllByText} = renderPageHistory(['/receitas-feitas']);
+    const firstRecipeShare = await findByTestId('0-horizontal-share-btn');
+    userEvent.click(firstRecipeShare);
+    expect(queryAllByText('Link copiado!')[0].hidden).toBeFalsy();
+    expect(copy).toHaveBeenCalledTimes(1);
+    const foodId = doneRecipes[0].id;
+    expect(copy).toHaveBeenCalledWith(`http://localhost:3000/comidas/${foodId}`)
+  });
+
+  test('58 - Implemente 2 botões que filtram as receitas por comida ou bebida e um terceiro que remove todos os filtros', async () => {
+    const {findByTestId} = renderPageHistory(['/receitas-feitas']);
+    const allBtn = await findByTestId('filter-by-all-btn');
+    const foodBtn = await findByTestId('filter-by-food-btn');
+    const drinkBtn = await findByTestId('filter-by-drink-btn');
+    userEvent.click(foodBtn);
+    const foodRecipeName = await findByTestId('0-horizontal-name');
+    expect(foodRecipeName.innerHTML).toBe(doneRecipes[0].name);
+    userEvent.click(drinkBtn);
+    const drinkRecipeName = await findByTestId('0-horizontal-name');
+    expect(drinkRecipeName.innerHTML).toBe(doneRecipes[1].name);
+    userEvent.click(allBtn);
+    const firstRecipe = await findByTestId('0-horizontal-name');
+    const secondRecipe = await findByTestId('1-horizontal-name');
+    expect(firstRecipe.innerHTML).toBe(doneRecipes[0].name);
+    expect(secondRecipe.innerHTML).toBe(doneRecipes[1].name);
+  });
+
+  test('59 - Redirecione para a tela de detalhes da receita caso seja clicado na foto ou no nome da receita', async () => {
+    const history = createBrowserHistory();
+    history.push('/receitas-feitas');
+    const {findByTestId} = render(
+      <Router history={ history }>
+        <FeitasFavoritas />
+      </Router>
+    );
+    const drinkCard = await findByTestId('1-horizontal-image');
+    const drinkId = doneRecipes[1].id;
+    userEvent.click(drinkCard);
+    expect(history.location.pathname).toBe(`/bebidas/${drinkId}`)
+  });
+});
+
+// describe('Favoritas', () => {
+//   beforeEach(() => {
+//     fetchMock();
+//     Object.defineProperty(window, 'localStorage', {
+//       value: {
+//         getItem: jest.fn(() => JSON.stringify(favoriteRecipes)),
+//         setItem: jest.fn(() => null),
+//         clear: jest.fn(() => null),
+//       },
+//       writable: true,
+//     });
+//   });
+
+//   test('', () => {
+//     const {} = renderPageHistory(['/receitas-favoritas']);
+//   });
+// });
---
+++ src/mocks/favoriteRecipes.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    id: '52771',
+    type: 'comida',
+    area: 'Italian',
+    category: 'Vegetarian',
+    alcoholicOrNot: '',
+    name: 'Spicy Arrabiata Penne',
+    image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+  },
+  {
+    id: '178319',
+    type: 'bebida',
+    area: '',
+    category: 'Cocktail',
+    alcoholicOrNot:  'Alcoholic',
+    name: 'Aquamarine',
+    image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
+  },
+];

bc398282500b7a9fa806ae6b9dc15a917aace3c3
Aline Debastiani <debastianialine@gmail.com>
2021-04-13 19:48:55 -0300

    Updates

---
+++ precache-manifest.d9b7fe132367aaee4c90ac419a6abe9a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4fa4122d7ca32e040e59a794cee6d85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14980a6421bbcfaf017b",
+    "url": "/static/css/2.3b12a38c.chunk.css"
+  },
+  {
+    "revision": "9631a206fd2dad44f8cb",
+    "url": "/static/css/main.6a26583b.chunk.css"
+  },
+  {
+    "revision": "14980a6421bbcfaf017b",
+    "url": "/static/js/2.c9d87e1c.chunk.js"
+  },
+  {
+    "revision": "9353b4df7938eafd5f8e3a0cf5a68c12",
+    "url": "/static/js/2.c9d87e1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9631a206fd2dad44f8cb",
+    "url": "/static/js/main.151e7ac3.chunk.js"
+  },
+  {
+    "revision": "3d4965c9977dafc48a8e",
+    "url": "/static/js/runtime-main.fce06d4f.js"
+  },
+  {
+    "revision": "cc80245d78fb6f7f4678328845608ad9",
+    "url": "/static/media/alimentos-ricos-em-fibras.jpg.cc80245d.webp"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "8652095e2890635da2c80826f30ea7dc",
+    "url": "/static/media/food-ingredients.8652095e.jpg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);

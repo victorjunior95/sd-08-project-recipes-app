4d971d0f17cc8948d1510ffd78308cf709b62c12
Fernanda PC <nandateixmarques@gmail.com>
2021-04-16 13:37:11 -0300

    arrumando erros do lint

--- src/containers/ExploreFoodsByIngredients.jsx
+++ src/containers/ExploreFoodsByIngredients.jsx
@@ -30,7 +30,6 @@ function ExploreFoodsByIngredients() {
               ingredientsData={ ingredient }
               index={ index }
               id={ ingredient.idIngredient }
-              className="img-link"
             />
           ))
         }
